fb482aa79c1a2ec3e3b986b7a22a75621bf9500a
Igor Pets <b_ear@mail.ru>
2022-08-12 00:38:31 +0300

    New task 1114

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1113/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1113/Solution.java
@@ -8,5 +8,6 @@ public class Solution {
     public static void main(String[] args) {
         if (System.getProperty("os.name").toLowerCase().contains("windows")) {
             System.out.println("Windows detected!");
+        }
     }
 }
