fcd783352f2b2edc39782abdc9a7490258624f7f
Igor Pets <b_ear@mail.ru>
2022-08-17 01:56:49 +0300

    New Task 1301 1302  open

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1222/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1222/Solution.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class Solution {
 
-    public static void printAnything(ArrayList<String> arrayList) {
+    public static void printAnything(ArrayList arrayList) {
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.println(arrayList.get(i));
         }
