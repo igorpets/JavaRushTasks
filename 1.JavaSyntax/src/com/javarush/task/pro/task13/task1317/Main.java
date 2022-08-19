7e5d38613eed628a769b7104c9382f8b7fb52bad
Igor Pets <b_ear@mail.ru>
2022-08-19 08:43:21 +0300

    New Task 1318 open

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1317/Main.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task13.task1317;
+
+public class Main {
+    public static void main(String[] args) {
+        Month month = Month.JANUARY;
+        System.out.println(month.ordinal());
+    }
+}
