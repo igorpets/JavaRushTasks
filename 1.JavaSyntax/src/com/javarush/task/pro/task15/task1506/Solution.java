15013c9d94594b5357cc8fd25b33562cbf5a9976
Igor Pets <b_ear@mail.ru>
2022-08-22 21:02:35 +0300

    New Task 1507 open

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task15.task1506;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -12,6 +13,20 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            List <String> lines = Files.readAllLines(Paths.get(scanner.nextLine()));
+            for(String text:lines) {
+                for (char symb: text.toCharArray()){
+                    if (symb !=',' && symb !='.' && symb !=' ')
+                        System.out.print(symb);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
     }
 }
 
