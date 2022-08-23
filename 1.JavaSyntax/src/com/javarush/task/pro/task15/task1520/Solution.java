dae207fbafc8bb023261ed3a664b07e7c320d5a0
Igor Pets <b_ear@mail.ru>
2022-08-23 12:56:04 +0300

    New Task Games: game2048 snake racer moonlander

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1520/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1520/Solution.java
@@ -1,4 +1,4 @@
-package com.javarush.task.pro.task15.task1519;
+package com.javarush.task.pro.task15.task1520;
 
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.util.Scanner;
 
 /* 
-Поверхностное копирование
+Перемещение файлов
 */
 
 public class Solution {
@@ -15,17 +15,20 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         Path sourceDirectory = Path.of(scanner.nextLine());
         Path targetDirectory = Path.of(scanner.nextLine());
+        //напишите тут ваш код
         if (!Files.isDirectory(sourceDirectory)) return;
         if (Files.isRegularFile(targetDirectory)) return;
         if (targetDirectory.equals(sourceDirectory)) return;
         //напишите тут ваш код
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(sourceDirectory)) {
             for (Path next_path : stream) {
+                System.out.println(next_path);
                 if (Files.isRegularFile(next_path)) {
                     Path new_path = targetDirectory.resolve(next_path.getFileName());
-                    Files.copy(next_path, new_path);
+                    Files.move(next_path, new_path);
                 }
             }
+        } catch (Exception e) {
         }
     }
 }
