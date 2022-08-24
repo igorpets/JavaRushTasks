7dbaa09316ee6873652a835993c8e5680c0ab126
Igor Pets <b_ear@mail.ru>
2022-08-24 10:53:50 +0300

    Task 1601 solved

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1601/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1601/Solution.java
@@ -1,23 +1,50 @@
 package com.javarush.task.pro.task16.task1601;
 
-import java.util.Calendar;
 import java.util.Date;
+
 
 /* 
 Лишь бы не в понедельник :)
 */
 
 public class Solution {
-
-    static Date birthDate;
+    static Date birthDate = new Date(1999-1900, 2, 12);
 
     public static void main(String[] args) {
         System.out.println(getDayOfWeek(birthDate));
     }
 
     static String getDayOfWeek(Date date) {
-        //напишите тут ваш код
-
-        return null;
+        String result = "";
+        //System.out.println("date = " + date);
+        //System.out.println("simple = "+new SimpleDateFormat("EEEE",Locale.forLanguageTag("ru")).format(date));
+        int day = date.getDay();
+        switch (day) {
+            case 0:
+                result = "воскресенье";break;
+            case 1:
+                result = "понедельник";break;
+            case 2:
+                result = "вторник";break;
+            case 3:
+                result = "среда";break;
+            case 4:
+                result = "четверг";break;
+            case 5:
+                result = "пятница";break;
+            case 6:
+                result = "суббота";break;
+        }
+        return result;
     }
 }
+        /* System.out.println(date+" day= "+date.getDay());
+        //напишите тут ваш код
+        int day = date.getDay();
+        DayOfWeek dayOfWeek = DayOfWeek.of(day+1);
+        Locale localeRu = new Locale("ru", "RU");
+        int day2 = Calendar.(1999, Calendar.MARCH, 12).get(Calendar.DAY_OF_MONTH)
+        System.out.println();
+
+        return dayOfWeek.getDisplayName(TextStyle.FULL,localeRu);
+        */
