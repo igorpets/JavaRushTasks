0b68f5bd40eda0205b6d998e79da28d659f16dc4
Igor Pets <b_ear@mail.ru>
2022-08-26 07:57:55 +0300

    Task 1620 solved 1621 loaded

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1620/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1620/Solution.java
@@ -5,6 +5,17 @@ import java.time.format.DateTimeFormatter;
 
 /* 
 Еще один простой шаблон
+4 9.3.19 06:03:07.319180500 Europe/Kiev
+Где:
+4 — номер дня недели, т.е. четверг;
+9 — день месяца;
+3 — месяц;
+19 — год;
+06 — часы;
+03 — минуты;
+07 — секунды;
+319180500 — наносекунды;
+Europe/Kiev — временная зона.
 */
 
 public class Solution {
@@ -13,5 +24,7 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("e d.M.yy HH:mm:ss.n VV");
+        System.out.println(f.format(zonedDateTime));
     }
 }
