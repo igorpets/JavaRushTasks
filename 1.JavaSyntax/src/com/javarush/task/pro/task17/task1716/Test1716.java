fd3c5dedb8b635867dfd9821c076be40f1b9dd91
Igor Pets <b_ear@mail.ru>
2022-08-27 18:05:54 +0300

    Task 1717 solved 1801 loaded

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1716/Test1716.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task17.task1716;
+
+public class Test1716 {
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        bus.start();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Test1717.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task17.task1717;
+
+public class Test1717 {
+    public static void main(String[] args) {
+        Aircraft air = new Aircraft();
+        System.out.println(air.getMaxSpeed());
+    }
+}
