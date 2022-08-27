e5ed3da998366af5ca45d0371b693b63a51bae21
Igor Pets <b_ear@mail.ru>
2022-08-27 10:43:05 +0300

    Task 1708 solved

--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1708/MinMaxUtil.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1708/MinMaxUtil.java
@@ -6,4 +6,46 @@ package com.javarush.task.pro.task17.task1708;
 
 public class MinMaxUtil {
     //напишите тут ваш код
+    public static void main(String[] args) {
+        System.out.println(min(2, 5));
+        System.out.println(min(7, 2, 5));
+        System.out.println(min(7, 10, 2, 5));
+        System.out.println(min(7, 2, 5, 15, 25));
+        System.out.println(max(25, 2, 5, 15, 25));
+        System.out.println(max(25, 2, 5, 15));
+        System.out.println(max(25, 2, 5));
+        System.out.println(max(25, 5));
+    }
+
+    public static int min(int a1, int a2) {
+        return Math.min(a1, a2);
+    }
+
+    public static int min(int a1, int a2, int a3) {
+        return min(min(a1, a2), a3);
+    }
+
+    public static int min(int a1, int a2, int a3, int a4) {
+        return min(min(a1, a2, a3), a4);
+    }
+
+    public static int min(int a1, int a2, int a3, int a4, int a5) {
+        return min(min(a1, a2, a3, a4), a5);
+    }
+
+    public static int max(int a1, int a2) {
+        return Math.max(a1, a2);
+    }
+
+    public static int max(int a1, int a2, int a3) {
+        return max(max(a1, a2), a3);
+    }
+
+    public static int max(int a1, int a2, int a3, int a4) {
+        return max(max(a1, a2, a3), a4);
+    }
+
+    public static int max(int a1, int a2, int a3, int a4, int a5) {
+        return max(max(a1, a2, a3, a4), a5);
+    }
 }
