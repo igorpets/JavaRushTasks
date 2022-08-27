e181ef4c632f3a42a4915cc0cc92d853d543c7df
Igor Pets <b_ear@mail.ru>
2022-08-27 10:32:17 +0300

    Task 1707 solved

--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Cat.java
@@ -4,4 +4,8 @@ public class Cat extends Pet {
     public static final String CAT = "Я не люблю людей.";
 
     //напишите тут ваш код
+    public void printInfo() {
+        super.printInfo();
+        System.out.println(CAT);
+    }
 }
--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Dog.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Dog.java
@@ -4,4 +4,8 @@ public class Dog extends Pet {
     public static final String DOG = "Я люблю людей.";
 
     //напишите тут ваш код
+    public void printInfo() {
+        super.printInfo();
+        System.out.println(DOG);
+    }
 }
