22ef8c22ce4eac7bc8cb736433d650a132a08383
Igor Pets <b_ear@mail.ru>
2022-08-28 15:39:29 +0300

    Task 1812 solved 1813 loaded

--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1812/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.pro.task18.task1812;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 /* 
@@ -30,6 +31,6 @@ public class Solution {
 
     public static Stream<Language> sortByRanking(ArrayList<Language> languages) {
         //напишите тут ваш код
-        return Stream.empty();
+        return languages.stream().sorted(Comparator.comparingDouble(Language::getRanking).reversed());
     }
 }
