5443a76180e1ce70dad01c99cf46f0e4b8a5b85c
Igor Pets <b_ear@mail.ru>
2022-08-28 13:45:32 +0300

    Task 1805 solved 1806 loaded

--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/Solution.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 Прощание с лямбда-выражением
 */
 
-final class Solution implements TestInterface{
+final class Solution {
 
     public static void main(String[] args) {
         var strings = new ArrayList<String>();
@@ -19,19 +19,8 @@ final class Solution implements TestInterface{
         for (String string : strings) {
             System.out.println(string);
         }
-        //short z=1.0;
-        char c='\u072f';
-        //String s = new String(75);
-        String _int,short7;
-        double z=0.5;
-        short x=4;
-        x= (short)(int)z;
     }
-    public int test(){
-        return 0;
-    }
-
     public static void sortStringsByLength(ArrayList<String> strings) {
-        Collections.sort(strings, (s1, s2) -> s1.length() - s2.length());
+        Collections.sort(strings, new StringComparator());
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/TestInterface.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/TestInterface.java
@@ -2,4 +2,13 @@ package com.javarush.task.pro.task18.task1805;
 
 interface TestInterface {
      int test();
+     default void test2(){
+          //short z=1.0;
+          char c='\u072f';
+          //String s = new String(75);
+          String _int,short7;
+          double z=0.5;
+          short x=4;
+          x= (short)(int)z;
+     }
 }
