67b673eacaf2642e3ac49213c81eebe9b3e7439b
Igor Pets <b_ear@mail.ru>
2022-09-01 00:40:31 +0300

    Task 1824 and step 01-03 minigames solved, Step 04 minigames loaded

--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1824/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1824/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.pro.task18.task1824;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toMap;
@@ -20,6 +21,6 @@ public class Solution {
 
     public static Map<String, Integer> getMap(Stream<String> stringStream) {
         //напишите тут ваш код
-        return new HashMap<>();
+        return stringStream.collect(Collectors.toMap(e->e, e->e.length()));
     }
 }
