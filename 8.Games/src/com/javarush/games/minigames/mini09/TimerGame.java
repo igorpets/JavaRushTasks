e984c7725767e34fd30e24bdeed02e688ff1f60f
Igor Pets <b_ear@mail.ru>
2022-09-03 00:46:47 +0300

    minigames step 09 solved, step 10 loaded +

--- 8.Games/src/com/javarush/games/minigames/mini09/TimerGame.java
+++ 8.Games/src/com/javarush/games/minigames/mini09/TimerGame.java
@@ -16,4 +16,11 @@ public class TimerGame extends Game {
     }
 
     //напишите тут ваш код
+    public void onTurn(int step){
+        setCellNumber(1,1,step);
+        Color clr = step%2==0?Color.GREEN:Color.ORANGE;
+        for(int x=0;x<3;x++)
+            for(int y=0;y<3;y++)
+                setCellColor(x,y, clr);
+    }
 }
