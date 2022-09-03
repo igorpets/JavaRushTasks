5c1ba814cd1617c1f80c93be3896e5efbf9410e1
Igor Pets <b_ear@mail.ru>
2022-09-03 14:01:45 +0300

    Task 1113 solved, 1114 loaded

--- 2.JavaCore/src/com/javarush/task/task11/task1113/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task11/task1113/Solution.java
@@ -12,15 +12,15 @@ public class Solution {
 
     }
 
-    public class Animal {
+    public class Animal extends Fish {
 
     }
 
-    public class Ape {
+    public class Ape extends Animal {
 
     }
 
-    public class Human {
+    public class Human extends Ape {
 
     }
 
