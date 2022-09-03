484188fd31a56c8b4e9f5a2e25a0b879697c7c21
Igor Pets <b_ear@mail.ru>
2022-09-03 08:19:43 +0300

    minigames step 10-13 solved

---
+++ 8.Games/src/com/javarush/games/minigames/mini11/FillOrangeGame.java
@@ -0,0 +1,12 @@
+package com.javarush.games.minigames.mini11;
+
+import com.javarush.engine.cell.Color;
+
+/* 
+Закрашиваем поле
+*/
+
+public class FillOrangeGame extends FillGame {
+
+    //напишите тут ваш код
+}
