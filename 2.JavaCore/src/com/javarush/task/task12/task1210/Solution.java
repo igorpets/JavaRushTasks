2380dc95dd52b94964164aa6226516338cda8411
Igor Pets <b_ear@mail.ru>
2022-09-03 16:17:50 +0300

    Task 1210 solved, 1211 loaded

--- 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -10,4 +10,15 @@ public class Solution {
     }
 
     //Напишите тут ваши методы
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static double max(double a, double b) {
+        return Math.max(a, b);
+    }
+
+    public static long max(long a, long b) {
+        return Math.max(a, b);
+    }
 }
