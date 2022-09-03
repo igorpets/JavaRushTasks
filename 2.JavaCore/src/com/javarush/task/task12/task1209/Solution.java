d236e980172b783aab4b5d07a9207a3dea544af5
Igor Pets <b_ear@mail.ru>
2022-09-03 16:13:47 +0300

    Task 1209 solved, 1210 loaded

--- 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -10,4 +10,15 @@ public class Solution {
     }
 
     //Напишите тут ваши методы
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public static double min(double a, double b) {
+        return Math.min(a, b);
+    }
+
+    public static long min(long a, long b) {
+        return Math.min(a, b);
+    }
 }
