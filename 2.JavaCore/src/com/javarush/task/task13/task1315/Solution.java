31bb5ec8e370e8e4e98cbba8ef48a433fe8100dc
Igor Pets <b_ear@mail.ru>
2022-09-04 22:33:15 +0300

    Task 1315 a solution has been prepared

--- 2.JavaCore/src/com/javarush/task/task13/task1315/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1315/Solution.java
@@ -2,6 +2,12 @@ package com.javarush.task.task13.task1315;
 
 /* 
 Том, Джерри и Спайк
+1. Класс Cat должен быть объявлен внутри класса Solution.
+2. Класс Dog должен быть объявлен внутри класса Solution.
+3. Класс Mouse должен быть объявлен внутри класса Solution.
+4. Кот (Cat) может передвигаться, может кого-то съесть и может быть сам съеден.
+5. Мышь (Mouse) может передвигаться и может быть съедена.
+6. Собака (Dog) может передвигаться и съесть кого-то.
 */
 
 public class Solution {
@@ -9,6 +15,19 @@ public class Solution {
 
     }
 
+    public class Cat implements Movable, Edible, Eat {
+        public void move() {}
+        public void beEaten() {}
+        public void eat() {}
+    }
+    public class Mouse implements Movable, Edible {
+        public void move() {}
+        public void beEaten() {}
+    }
+    public class Dog implements Movable, Eat {
+        public void move() {}
+        public void eat() {}
+    }
     //может двигаться
     public interface Movable {
         void move();
