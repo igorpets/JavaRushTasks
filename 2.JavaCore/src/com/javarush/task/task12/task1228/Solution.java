bc29848389c8a3c679d94dfaa3b3c1475702d0c7
Igor Pets <b_ear@mail.ru>
2022-09-04 12:05:26 +0300

    Task 1228 solved, 1229 loaded

--- 2.JavaCore/src/com/javarush/task/task12/task1228/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1228/Solution.java
@@ -26,7 +26,7 @@ public class Solution {
         public void workVeryHard();
     }
 
-    public static class Human {
+    public static class Human implements Worker, Businessman, Secretary {
 
         public void workHard() {
         }
