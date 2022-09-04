ad997256ddeaaad4b237421518eb03ee53f79ad9
Igor Pets <b_ear@mail.ru>
2022-09-04 12:11:30 +0300

    Task 1229 solved, 1230 loaded

--- 2.JavaCore/src/com/javarush/task/task12/task1229/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1229/Solution.java
@@ -2,6 +2,13 @@ package com.javarush.task.task12.task1229;
 
 /* 
 Родитель класса CTO
+1. Класс Solution должен содержать интерфейс Businessman с методом void workHard().
+2. Класс Solution должен содержать не абстрактный класс CTO.
+3. Класс CTO должен реализовывать интерфейс Businessman.
+4. Класс CTO не должен содержать методов.
+5. Класс Solution должен содержать дополнительный класс.
+6. Класс CTO должен наследоваться от дополнительного класса.
+7. Дополнительный класс должен содержать один метод.
 */
 
 public class Solution {
@@ -15,7 +22,12 @@ public class Solution {
         public void workHard();
     }
 
-    public static class CTO implements Businessman {
+    public static class Owner {
+        public void workHard(){
+            System.out.println("HARD!");
+        }
+    }
+    public static class CTO extends Owner implements Businessman {
 
     }
 }
