5409a8fa7044f389cd47126bff3eb34a6d07be3b
Igor Pets <b_ear@mail.ru>
2022-09-04 23:14:01 +0300

    Task 1316 a solution has been prepared

--- 2.JavaCore/src/com/javarush/task/task13/task1316/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1316/Solution.java
@@ -2,6 +2,11 @@ package com.javarush.task.task13.task1316;
 
 /* 
 Некорректные строки
+1. Интерфейс SimpleObject должен быть объявлен внутри класса Solution.
+2. Интерфейс Button должен быть объявлен внутри класса Solution.
+3. Метод main менять нельзя.
+4. В интерфейсе Button должно остаться объявление константы NAME.
+5. В интерфейсе Button должно остаться корректное объявление метода onPress.
 */
 
 public class Solution {
@@ -18,19 +23,19 @@ public class Solution {
 
         final String NAME = "Submit";
 
-        public void onPress();
+        //public void onPress();
 
-        protected void onPress();
+        //protected void onPress();
 
         void onPress();
 
-        private void onPress();
+        //private void onPress();
 
-        protected String onPress(Object o);
+        //protected String onPress(Object o);
 
         String onPress(Object o);
 
-        private String onPress(Object o);
+        //private String onPress(Object o);
 
     }
 }
