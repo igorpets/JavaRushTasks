c2d30a8b79e6fdc38f13c16636f610829cbc3cee
Igor Pets <b_ear@mail.ru>
2022-09-06 20:42:15 +0300

    Task 1326 solved, 1327 loaded

--- 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task13.task1326;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,6 +15,27 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // напишите тут ваш код
+        try (Scanner scanner = new Scanner(System.in);
+             FileInputStream input = new FileInputStream(scanner.nextLine());
+             BufferedReader br = new BufferedReader(new InputStreamReader(input))) {
+            String line;
+            ArrayList<Integer> list = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                Integer val = null;
+                try {
+                    val = Integer.valueOf(line);
+                } catch (Exception e) {
+                    continue;
+                }
+                if (Math.abs(val) % 2 == 0)
+                    list.add(val);
+            }
+            list.sort((a,b)->a-b);
+            for (Integer num: list) {
+                System.out.println(num);
+            }
+        } catch (Exception e) {
 
+        }
     }
 }
