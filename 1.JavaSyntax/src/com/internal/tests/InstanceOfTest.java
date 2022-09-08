e983a8cb590d881e2e092839c87edad76250bc8c
Igor P <b_ear@mail.ru>
2022-09-08 18:14:30 +0300

    Task 1408-1421 loaded

---
+++ 1.JavaSyntax/src/com/internal/tests/InstanceOfTest.java
@@ -0,0 +1,10 @@
+package com.internal.tests;
+
+public class InstanceOfTest {
+    public static void main(String[] args) {
+
+        Integer x = new Integer(22);
+        System.out.println(x);
+        //System.out.println(x instanceof String);
+    }
+}
