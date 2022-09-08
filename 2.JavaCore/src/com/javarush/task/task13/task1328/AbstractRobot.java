6e7aebd40d0ca8f7c35b966f859063eadedf789b
Demichev Andrey <a_demichev@eureca.ru>
2022-09-08 15:04:13 +0300

    Task 1328 AbstractRobot is abstract now.

--- 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -1,4 +1,8 @@
 package com.javarush.task.task13.task1328;
 
-public class AbstractRobot {
+/**
+ * 1. Класс AbstractRobot должен быть абстрактным.
+ * 2. Класс AbstractRobot должен реализовывать интерфейсы Attackable и Defensable.
+ */
+public abstract class AbstractRobot implements  Attackable, Defensable {
 }
