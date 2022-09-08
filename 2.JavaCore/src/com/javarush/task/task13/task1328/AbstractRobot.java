2354cfa279af7ccb1d46f024117cf101e43a1bba
Igor P <b_ear@mail.ru>
2022-09-08 16:08:12 +0300

    Task 1328 solved, 1401 loaded

--- 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -4,9 +4,11 @@ package com.javarush.task.task13.task1328;
  * 1. Класс AbstractRobot должен быть абстрактным.
  * 2. Класс AbstractRobot должен реализовывать интерфейсы Attackable и Defensable.
  */
-public abstract class AbstractRobot implements  Attackable, Defensable {
-    private static int hitCount;
+public abstract class AbstractRobot implements Attackable, Defensable {
+    private static int hitCount = 0;
+
     abstract String getName();
+
     public BodyPart attack() {
         BodyPart attackedBodyPart = null;
         hitCount = hitCount + 1;
@@ -16,6 +18,8 @@ public abstract class AbstractRobot implements  Attackable, Defensable {
         } else if (hitCount == 2) {
             attackedBodyPart = BodyPart.HEAD;
         } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.CHEST;
+        } else {
             hitCount = 0;
             attackedBodyPart = BodyPart.LEG;
         }
@@ -31,6 +35,8 @@ public abstract class AbstractRobot implements  Attackable, Defensable {
         } else if (hitCount == 2) {
             defendedBodyPart = BodyPart.LEG;
         } else if (hitCount == 3) {
+            defendedBodyPart = BodyPart.CHEST;
+        } else {
             hitCount = 0;
             defendedBodyPart = BodyPart.ARM;
         }
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1401/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task14.task1401;
+
+/* 
+Bingo!
+
+3. В переменной Object animal должен храниться объект, который будет одновременно являться и Pet, и Cat, и Tiger.
+4. Программа должна выводить на экран: "Bingo!".
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Object animal = new Tiger();
+        boolean isCat = animal instanceof Cat;
+        boolean isTiger = animal instanceof Tiger;
+        boolean isPet = animal instanceof Pet;
+
+        printResults(isCat, isTiger, isPet);
+    }
+
+    private static void printResults(boolean cat, boolean tiger, boolean pet) {
+        if (cat && tiger && pet) System.out.println("Bingo!");
+    }
+
+    static class Pet {
+    }
+
+    static class Cat extends Pet {
+    }
+
+    static class Tiger extends Cat {
+    }
+
+}
