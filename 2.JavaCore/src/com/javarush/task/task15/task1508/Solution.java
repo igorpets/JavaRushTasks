b6dda165e9d6c8a23af3c63ad440792a6c6a3cfb
Igor Pets <b_ear@mail.ru>
2022-09-10 17:59:59 +0300

    Task 1508 solved, 1509 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1508/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1508/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task15.task1508;
 
 /* 
 ООП - Перегрузка - убираем лишнее
+1. В классе Solution кроме метода main должен остаться только один метод print (с правильным параметром).
+4. Программа должна выводить на экран строку "Я буду Java прогером!".
 */
 
 public class Solution {
@@ -9,23 +11,23 @@ public class Solution {
         print(1);
     }
 
-    public static void print(byte b) {
-        System.out.println("Я буду Java прогером!");
-    }
+    /*public static void print(byte b) {
+        System.out.println("Я буду Java прогером!1");
+    }*/
 
-    public static void print(double d) {
-        System.out.println("Я буду Java прогером!");
-    }
+    /*public static void print(double d) {
+        System.out.println("Я буду Java прогером!2");
+    }*/
 
     public static void print(long l) {
         System.out.println("Я буду Java прогером!");
     }
 
-    public static void print(float f) {
-        System.out.println("Я буду Java прогером!");
-    }
+    /*public static void print(float f) {
+        System.out.println("Я буду Java прогером!4");
+    }*/
 
-    public static void print(char c) {
-        System.out.println("Я буду Java прогером!");
-    }
+    /*public static void print(char c) {
+        System.out.println("Я буду Java прогером!5");
+    }*/
 }
