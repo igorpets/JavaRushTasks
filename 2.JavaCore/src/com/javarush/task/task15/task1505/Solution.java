6f42fe27adf25dfcb22c0860ad72c6f88144e641
Igor Pets <b_ear@mail.ru>
2022-09-10 17:34:54 +0300

    Task 1505 solved, 1506 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1505/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1505/Solution.java
@@ -6,23 +6,27 @@ package com.javarush.task.task15.task1505;
 
 public class Solution {
     public interface HasWeight {
-        int getValue();
+        int getWeight();
     }
 
     public interface HasHeight {
-        int getValue();
+        int getHeight();
     }
 
     public static class Human implements HasWeight, HasHeight {
         @Override
-        public int getValue() {
-            return 0;
+        public int getWeight() {
+            return 65;
+        }
+        @Override
+        public int getHeight() {
+            return 190;
         }
     }
 
     public static void main(String[] args) {
         Human human = new Human();
-        System.out.println(human.getValue());
-        System.out.println(human.getValue());
+        System.out.println(human.getWeight());
+        System.out.println(human.getHeight());
     }
 }
