74a3ece4c1cbf46bca125f5f2cbf87bc620ee082
Igor Pets <b_ear@mail.ru>
2022-09-10 16:29:56 +0300

    Task 1503 solved, 1504 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1503/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1503/Solution.java
@@ -12,15 +12,17 @@ public class Solution {
         new Solution.Lanos().printlnDesire();
     }
 
-    public static class Ferrari {
+    public static class Ferrari extends LuxuriousCar{
         public void printlnDesire() {
             //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.FERRARI_NAME);
         }
     }
 
-    public static class Lanos {
+    public static class Lanos extends CheapCar{
         public void printlnDesire() {
             //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.LANOS_NAME);
         }
     }
 
@@ -30,5 +32,17 @@ public class Solution {
         public static String CHEAP_CAR = "дешевой машине";
         public static String FERRARI_NAME = "Феррари";
         public static String LANOS_NAME = "Ланосе";
+    }
+    public static class LuxuriousCar {
+        void printlnDesire() {
+            //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.LUXURIOUS_CAR);
+        }
+    }
+    public static class CheapCar {
+        void printlnDesire() {
+            //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.CHEAP_CAR);
+        }
     }
 }
