63001ab427a1aa39e4e58d044d210c2e9fe55bc0
Igor Pets <b_ear@mail.ru>
2022-09-10 08:21:08 +0300

    Task 1414 solved

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task14.task1414;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /* 
 MovieFactory
@@ -10,13 +12,22 @@ MovieFactory
 public class Solution {
     public static void main(String[] args) throws Exception {
         //ввести с консоли несколько ключей (строк), пункт 7
-
+        try (Scanner scanner = new Scanner(System.in)) {
         /*
-8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
-8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
-8.2 вывести на экран movie.getClass().getSimpleName()
+            8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+            8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+            8.2 вывести на экран movie.getClass().getSimpleName()
         */
-
+            while (true) {
+                String key = scanner.nextLine();
+                Movie mov = MovieFactory.getMovie(key);
+                if (mov == null) {
+                    break;
+                } else {
+                    System.out.println(mov.getClass().getSimpleName());
+                }
+            }
+        }
     }
 
     static class MovieFactory {
@@ -28,9 +39,12 @@ public class Solution {
             if ("soapOpera".equals(key)) {
                 movie = new SoapOpera();
             }
-
             //напишите тут ваш код, пункты 5,6
-
+            else if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            } else if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
             return movie;
         }
     }
@@ -42,4 +56,6 @@ public class Solution {
     }
 
     //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie{}
+    static class Thriller extends Movie{}
 }
