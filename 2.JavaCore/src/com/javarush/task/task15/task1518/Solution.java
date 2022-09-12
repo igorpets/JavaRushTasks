496d831f86d2f7a1129f75650c1ec0add100944a
Igor Pets <b_ear@mail.ru>
2022-09-12 22:32:18 +0300

    Task 1518 solved, 1529 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1518/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1518/Solution.java
@@ -6,8 +6,16 @@ package com.javarush.task.task15.task1518;
 
 public class Solution {
     public static Cat cat;
+    static {
+        cat = new Cat();
+        cat.name = "Кот";
+        System.out.println(cat.name);
+    }
 
     public static void main(String[] args) {
 
+    }
+    public static class Cat {
+        public String name;
     }
 }
