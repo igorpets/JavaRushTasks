686dc0d8cd818ff24ac27668422392030b1188a7
Igor Pets <b_ear@mail.ru>
2022-09-12 21:50:29 +0300

    Task 1517 solved, 1518 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1517/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1517/Solution.java
@@ -4,11 +4,15 @@ package com.javarush.task.task15.task1517;
 Статики и исключения
 */
 
+import javax.management.RuntimeErrorException;
+
 public class Solution {
     public static int A = 0;
 
     static {
         //throw an exception here - выбросьте эксепшн тут
+        int[] aa= new int[5];
+        if (2==2) System.out.println(aa[10]);
     }
 
     public static int B = 5;
