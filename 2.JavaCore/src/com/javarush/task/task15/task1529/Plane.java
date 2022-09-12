e4d82970967e09c6d46ff880115c18aa021cc6bf
Igor Pets <b_ear@mail.ru>
2022-09-12 23:27:46 +0300

    Task 1529 solved, 1530 loaded

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+    public Plane(int n) {
+
+    }
+    public void fly(){}
+}
--- 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
@@ -3,9 +3,13 @@ package com.javarush.task.task15.task1529;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Осваивание статического блока
+6. Если введенная строка равна "helicopter", в переменную result должен быть сохранен объект типа Helicopter.
+7. Если введенная строка равна "plane", в переменную result должен быть сохранен объект типа Plane.
+8. Поле result класса Solution должно быть инициализировано в статическом блоке путем вызова метода reset().
 */
 
 public class Solution {
@@ -15,11 +19,19 @@ public class Solution {
 
     static {
         //add your code here - добавьте код тут
+        reset();
     }
 
     public static CanFly result;
 
     public static void reset() {
         //add your code here - добавьте код тут
+        try (Scanner scan = new Scanner(System.in)) {
+            String line = scan.nextLine();
+            if (line.equals("helicopter")) result = new Helicopter();
+            else if (line.equals("plane")) result = new Plane(1);
+        } catch(Exception e) {
+
+        }
     }
 }
