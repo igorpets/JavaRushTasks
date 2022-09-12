592c53e6f4fe6c34a85dca977dc4b124a31ae7f1
Igor Pets <b_ear@mail.ru>
2022-09-12 21:46:09 +0300

    Task 1516 solved, 1517 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1516/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1516/Solution.java
@@ -5,6 +5,31 @@ package com.javarush.task.task15.task1516;
 */
 
 public class Solution {
+    public int intVar;
+    public double doubleVar;
+    public Double DoubleVar;
+    public boolean booleanVar;
+    public Object ObjectVar;
+    public Exception ExceptionVar;
+    public String StringVar;
+
     public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.intVar);
+        System.out.println(sol.doubleVar);
+        System.out.println(sol.DoubleVar);
+        System.out.println(sol.booleanVar);
+        System.out.println(sol.ObjectVar);
+        System.out.println(sol.ExceptionVar);
+        System.out.println(sol.StringVar);
+        /*switch(args[0]) {
+            case "-c":
+                if (args[2].equals("м")) {
+                    System.out.println("OK");
+                } else if (args[2].equals("ж")) {
+                    System.out.println("NOT OK");
+                }
+                break;
+        }*/
     }
 }
