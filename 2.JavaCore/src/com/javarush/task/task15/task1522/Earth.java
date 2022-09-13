e2eb18781213b913b3813c46349bd2c8b960d233
Igor Pets <b_ear@mail.ru>
2022-09-13 09:16:45 +0300

    Task 1522 solved, 1523 loaded

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Earth.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1522;
+
+public class Earth implements Planet{
+    private static volatile Earth instance;
+
+    public static Earth getInstance() {
+        Earth localInstance = instance;
+        if (localInstance == null) {
+            synchronized (Earth.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new Earth();
+                }
+            }
+        }
+        return localInstance;
+    }
+    private Earth(){
+        super();
+    }
+
+}
--- 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
@@ -3,12 +3,18 @@ package com.javarush.task.task15.task1522;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Закрепляем паттерн Singleton
+16. Метод readKeyFromConsoleAndInitPlanet() должен быть вызван в статическом блоке класса Solution.
+17. Метод readKeyFromConsoleAndInitPlanet() должен считывать одну строку с клавиатуры.
+18. Метод readKeyFromConsoleAndInitPlanet() должен корректно обновлять значение переменной thePlanet в соответствии с условием задачи.
+19. Классы Sun, Moon и Earth должны быть созданы в отдельных файлах.
 */
 
 public class Solution {
+
     public static void main(String[] args) {
 
     }
@@ -16,8 +22,28 @@ public class Solution {
     public static Planet thePlanet;
 
     //add static block here - добавьте статический блок тут
+    static {
+        readKeyFromConsoleAndInitPlanet();
+    }
 
     public static void readKeyFromConsoleAndInitPlanet() {
         // implement step #5 here - реализуйте задание №5 тут
+        try (Scanner scan = new Scanner(System.in)) {
+            String line = scan.nextLine();
+            switch (line) {
+                case Planet.MOON:
+                    thePlanet = Moon.getInstance();
+                    break;
+                case Planet.EARTH:
+                    thePlanet = Earth.getInstance();
+                    break;
+                case Planet.SUN:
+                    thePlanet = Sun.getInstance();
+                    break;
+                default:
+                    thePlanet = null;
+                    break;
+            }
+        }
     }
 }
