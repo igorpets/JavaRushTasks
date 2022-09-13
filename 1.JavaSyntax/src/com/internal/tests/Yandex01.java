600d88e60393935ec51647027c3f4f0e77f38f01
Пец Игорь Эдуардович <iepec@Eureca-corp.ru>
2022-09-13 18:42:51 +0300

    yandex02-03 tests

--- 1.JavaSyntax/src/com/internal/tests/Yandex01.java
+++ 1.JavaSyntax/src/com/internal/tests/Yandex01.java
@@ -1,24 +1,18 @@
 package com.internal.tests;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Yandex01 {
     public static void main(String[] args) {
-        int A;
-        int B;
-        while (true) {
-            try (Scanner scan = new Scanner(System.in)) {
-                String line = scan.nextLine();
-                if (line.indexOf(' ') < 0) {
-                    String[] list = line.split(" ");
-                    if (list.length == 2) {
-                        A = Integer.parseInt(list[0]);
-                        B = Integer.parseInt(list[1]);
-                        break;
-                    }
-                }
-            }
+        int A = 0;
+        int B = 0;
+        String line;
+        try (Scanner scan = new Scanner(System.in)) {
+            A = scan.nextInt();
+            B = scan.nextInt();
+        } catch (Exception e) {
         }
+        int res = A + B;
+        System.out.println(String.valueOf(res));
     }
 }
