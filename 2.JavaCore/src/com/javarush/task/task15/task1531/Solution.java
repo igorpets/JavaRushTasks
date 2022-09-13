c6fcdc93d632c76465af540da323c10de6f6cdc5
Igor Pets <b_ear@mail.ru>
2022-09-13 07:49:56 +0300

    Task 1531 solved, 1519 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
@@ -7,6 +7,10 @@ import java.math.BigDecimal;
 
 /* 
 Факториал
+1. Программа должна считывать данные с клавиатуры.
+2. Программа должна выводить на экран факториал введенного числа.
+3. Метод factorial должен возвращать строковое представление факториала числа, переданного ему в качестве параметра.
+4. Метод factorial должен принимать один параметр типа int.
 */
 
 public class Solution {
@@ -21,7 +25,15 @@ public class Solution {
 
     public static String factorial(int n) {
         //add your code here
-
-        return "";
+        String res;
+        if (n<0 || n > 150) res = "0";
+        else if (n == 0) res = "1";
+        else {
+            BigDecimal frac = new BigDecimal(1);
+            for (int i = 2; i <= n; i++)
+                frac = frac.multiply(new BigDecimal(i));
+            res = frac.toString();
+        }
+        return res;
     }
 }
