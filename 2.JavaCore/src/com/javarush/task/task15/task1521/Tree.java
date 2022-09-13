5f657d4e78ea96644a837916ca57ab9673706ca4
Igor Pets <b_ear@mail.ru>
2022-09-13 08:38:39 +0300

    Task 1519 1521 solved, 1522 loaded

--- 2.JavaCore/src/com/javarush/task/task15/task1521/Tree.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1521/Tree.java
@@ -1,11 +1,6 @@
 package com.javarush.task.task15.task1521;
 
 /*
-1. В классе Tree должен быть реализован метод info(Object s).
-2. В классе Tree должен быть реализован метод info(Number s).
-3. В классе Tree должен быть реализован метод info(String s).
-4. Метод info(Number s) должен выводить на экран строку, аналогичную строке из метода info(Object s), заменив лишь фразу("метод Object" на "метод Number").
-5. Метод info(String s) должен выводить на экран строку, аналогичную строке из метода info(Object s), заменив лишь фразу("метод Object" на "метод String").
 */
 public class Tree {
     public static int globalNumber;
@@ -17,5 +12,11 @@ public class Tree {
 
     public void info(Object s) {
         System.out.println(String.format("Дерево № %d , метод Object, параметр %s", number, s.getClass().getSimpleName()));
+    }
+    public void info(Number s) {
+        System.out.println(String.format("Дерево № %d , метод Number, параметр %s", number, s.getClass().getSimpleName()));
+    }
+    public void info(String s) {
+        System.out.println(String.format("Дерево № %d , метод String, параметр %s", number, s.getClass().getSimpleName()));
     }
 }
