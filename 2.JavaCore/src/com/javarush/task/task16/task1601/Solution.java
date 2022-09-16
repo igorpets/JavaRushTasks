0770bb4ef66cb3ed57c6f61df01a4d9ad8556d35
Igor Pets <b_ear@mail.ru>
2022-09-16 23:23:20 +0300

    Task 1601 solved, 1602 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
@@ -2,11 +2,21 @@ package com.javarush.task.task16.task1601;
 
 /* 
 My first thread
+1. Добавь в класс Solution публичный статический класс TestThread.
+2. Класс TestThread должен реализовывать интерфейс Runnable.
+3. Метод run класса TestThread должен выводить "My first thread".
+4. Программа должна вывести "My first thread".
+5. Метод main не изменять.
 */
 
 public class Solution {
     public static void main(String[] args) {
         TestThread task = new TestThread();
         new Thread(task).start();
+    }
+    public static class TestThread implements Runnable{
+        public void run() {
+            System.out.println("My first thread");
+        }
     }
 }
