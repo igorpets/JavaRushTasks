4e53b0e02c79d985027c28ac876906c9e9818649
Igor Pets <b_ear@mail.ru>
2022-09-17 22:24:07 +0300

    task 1603 solved 1604 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 /* 
 Список и нити
+3. Добавь 5 разных нитей в список list.
+4. Каждая нить из списка list должна работать со своим объектом класса SpecialThread.
+5. Класс SpecialThread изменять нельзя.
 */
 
 public class Solution {
@@ -12,6 +15,21 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        SpecialThread st1 = new SpecialThread();
+        SpecialThread st2 = new SpecialThread();
+        SpecialThread st3 = new SpecialThread();
+        SpecialThread st4 = new SpecialThread();
+        SpecialThread st5 = new SpecialThread();
+        Thread th1 = new Thread(st1);
+        Thread th2 = new Thread(st2);
+        Thread th3 = new Thread(st3);
+        Thread th4 = new Thread(st4);
+        Thread th5 = new Thread(st5);
+        list.add(th1);
+        list.add(th2);
+        list.add(th3);
+        list.add(th4);
+        list.add(th5);
     }
 
     public static class SpecialThread implements Runnable {
