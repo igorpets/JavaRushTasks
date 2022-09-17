56e0386eff0362c0568db9f65942403fdacdc80f
Igor Pets <b_ear@mail.ru>
2022-09-17 23:01:06 +0300

    task 1604 solved 1605 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1604/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1604/Solution.java
@@ -19,4 +19,11 @@ public class Solution {
             System.out.println(element);
         }
     }
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+                System.out.println(element);
+            }
+        }
+    }
 }
