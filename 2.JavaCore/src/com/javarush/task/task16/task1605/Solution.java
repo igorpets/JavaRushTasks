aa334e61b895a00059a009f833775c81543d5f8f
Igor Pets <b_ear@mail.ru>
2022-09-19 00:51:38 +0300

    Task 1605 solved, 1606 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1605/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1605/Solution.java
@@ -4,6 +4,17 @@ import java.util.Date;
 
 /* 
 Поговорим о музыке
+1. Класс Violin не должен быть унаследован от какого-либо дополнительного класса.
+2. Класс Violin должен реализовывать интерфейс MusicalInstrument.
+3. Метод run класса Violin должен вызывать метод startPlaying.
+4. Метод run класса Violin должен вызывать метод sleepNSeconds с параметром 1 секунда.
+5. Метод run класса Violin должен вызывать метод stopPlaying.
+6. Метод run класса Violin должен выводить на консоль продолжительность игры в миллисекундах. Используй формат из примера.
+
+1. Считай время начала игры - метод startPlaying().
+2. Подожди 1 секунду - метод sleepNSeconds(int n), где n - количество секунд.
+3. Считай время окончания игры - метод stopPlaying().
+4. Выведи на консоль продолжительность игры в миллисекундах. Используй методы из пунктов 2.1 и 2.3.
 */
 
 public class Solution {
@@ -28,7 +39,7 @@ public class Solution {
         Date stopPlaying();
     }
 
-    public static class Violin {
+    public static class Violin implements MusicalInstrument{
         private String owner;
 
         public Violin(String owner) {
@@ -43,6 +54,13 @@ public class Solution {
         public Date stopPlaying() {
             System.out.println(this.owner + " is stopping playing");
             return new Date();
+        }
+        public void run(){
+            Date start = startPlaying();
+            sleepNSeconds(1);
+            Date stop = stopPlaying();
+            long res = stop.getTime() - start.getTime();
+            System.out.println( "Playing "+res+ " ms");
         }
     }
 }
