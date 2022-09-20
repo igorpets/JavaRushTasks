59235939daddd16976a18214ee5264f3535a79f5
Igor Pets <b_ear@mail.ru>
2022-09-21 01:07:23 +0300

    Task 1626 solved, 1629 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
@@ -2,6 +2,13 @@ package com.javarush.task.task16.task1626;
 
 /* 
 Создание по образцу
+По образу и подобию CountdownRunnable создай нить CountUpRunnable, которая выводит значения в нормальном порядке - от 1 до number.
+
+1. Класс CountUpRunnable должен реализовывать интерфейс Runnable.
+2. Класс CountUpRunnable должен иметь публичный метод run без параметров.
+3. Метод run класса CountUpRunnable должен работать примерно 2,5 секунды.
+4. Метод run класса CountUpRunnable должен каждые полсекунды выводить имя потока, двоеточие и значение счетчика от 1 до 5 (например: "Увеличиваем: 1").
+5. Метод run класса CountUpRunnable должен вызывать Thread.sleep(500).
 */
 
 public class Solution {
@@ -12,8 +19,25 @@ public class Solution {
         new Thread(new CountUpRunnable(), "Увеличиваем").start();
     }
 
-    public static class CountUpRunnable {
+    public static class CountUpRunnable implements Runnable{
         //Add your code here - добавь код тут
+        private int countIndexUp = 0;
+
+        public void run() {
+            try {
+                while (true) {
+                    countIndexUp++;
+                    System.out.println(toString());
+                    Thread.sleep(500);
+                    if (countIndexUp == Solution.number) return;
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
     }
 
 
