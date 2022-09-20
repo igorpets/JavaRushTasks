4dfa00045aadd3c51bfb713def4746ec45d0cb23
Igor P <b_ear@mail.ru>
2022-09-20 20:33:29 +0300

    Task 1619 solved 1620 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -2,7 +2,8 @@ package com.javarush.task.task16.task1619;
 
 /* 
 А без interrupt слабо?
-Реализуй метод ourInterruptMethod таким образом, чтобы он прерывал нить TestThread. Исправь остальной код программы, если это необходимо. Нельзя использовать метод interrupt.
+Реализуй метод ourInterruptMethod таким образом, чтобы он прерывал нить TestThread.
+Исправь остальной код программы, если это необходимо. Нельзя использовать метод interrupt.
 
 1. В классе Solution должен быть публичный статический метод ourInterruptMethod без параметров.
 2. Метод run должен выводить надпись "he-he" каждые пол секунды, пока не будет вызван метод ourInterruptMethod.
@@ -13,6 +14,8 @@ package com.javarush.task.task16.task1619;
 */
 
 public class Solution {
+    private static boolean isCancel = false;
+
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(new TestThread());
         t.start();
@@ -21,12 +24,12 @@ public class Solution {
     }
 
     public static void ourInterruptMethod() {
-
+        isCancel = true;
     }
 
     public static class TestThread implements Runnable {
         public void run() {
-            while (true) {
+            while (!isCancel) {
                 try {
                     System.out.println("he-he");
                     Thread.sleep(500);
