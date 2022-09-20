c3ca0d94854e9aa9dffdf90205986e5809db0467
Igor Pets <b_ear@mail.ru>
2022-09-21 00:32:38 +0300

    Task 1625 solved, 1622 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1625/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1625/Solution.java
@@ -2,6 +2,18 @@ package com.javarush.task.task16.task1625;
 
 /* 
 Взаимная блокировка
+2. Не меняя классы T1 и T2 сделай так, чтобы их нити завершились, не обязательно успешно.
+3. Метод sleep не использовать.
+
+
+Requirements:
+1. Метод main должен запускать нить t1.
+2. Метод main должен запускать нить t2.
+3. Класс T1 не изменять.
+4. Класс T2 не изменять.
+5. Метод sleep не использовать.
+6. Вывод программы должен состоять из 2х строк, информирующих о завершении нитей. Например: "T1 was interrupted" и "T2 finished".
+7. Нити t1 и t2 должны завершаться (не обязательно успешно).
 */
 
 public class Solution {
@@ -11,6 +23,7 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         t1.start();
         t2.start();
+        t2.interrupt();
     }
 
     public static class T1 extends Thread {
