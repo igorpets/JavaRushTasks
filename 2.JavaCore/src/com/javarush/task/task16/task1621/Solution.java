87a084b40057410777bb49a7236c3ce8dcb4a4d2
Igor Pets <b_ear@mail.ru>
2022-09-21 00:28:21 +0300

    Task 1621 solved, 1625 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1621/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1621/Solution.java
@@ -2,6 +2,17 @@ package com.javarush.task.task16.task1621;
 
 /* 
 Thread.currentThread - всегда возвращает текущую нить
+1. В методе printMsg присвой переменной t текущую нить.
+2. В методе printMsg после всех действий поставь задержку в 1 миллисекунду.
+
+
+Requirements:
+1. Метод printMsg должен получать текущую нить с помощью Thread.currentThread.
+2. Метод printMsg должен усыплять нить на 1 миллисекунду.
+3. Метод printMsg должен вызывать метод getName у текущей нити.
+4. Метод main должен вызвать метод printMsg у объекта типа ThreadNamePrinter 5 раз.
+5. Метод run должен вызвать метод printMsg 5 раз.
+6. Метод printMsg у объекта типа ThreadNamePrinter суммарно должен быть вызван 10 раз.
 */
 
 public class Solution {
@@ -23,10 +34,13 @@ public class Solution {
         }
 
         public void printMsg() {
-            Thread t = null;//присвой переменной t текущую нить
+            Thread t = Thread.currentThread();//присвой переменной t текущую нить
             String name = t.getName();
             System.out.println("Name=" + name);
             //add sleep here - добавь sleep тут
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {}
         }
     }
 }
