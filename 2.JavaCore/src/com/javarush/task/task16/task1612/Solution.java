02c6a37ffbb377537dff806909a962a0ce288191
Igor P <b_ear@mail.ru>
2022-09-20 08:10:43 +0300

    Task 1612 solved 1613 loaded

--- 2.JavaCore/src/com/javarush/task/task16/task1612/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1612/Solution.java
@@ -40,6 +40,7 @@ public class Solution {
         private void doStep() throws InterruptedException {
             stepNumber++;
             //add your code here - добавь код тут
+            Thread.sleep(1000/owner.getSpeed());
             System.out.println(owner.getName() + " делает шаг №" + stepNumber + "!");
         }
     }
