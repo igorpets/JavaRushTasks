a1faeda69e99ac25a18467b547cdb77e417e1011
Igor Pets <b_ear@mail.ru>
2022-09-24 21:31:22 +0300

    Task 1704 solved 1705 loaded

--- 2.JavaCore/src/com/javarush/task/task17/task1704/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1704/Solution.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 /* 
 Синхронизированные заметки 2
+
+Класс Note будет использоваться нитями. Поэтому сделай так, чтобы вcе методы были синхронизированы.
+1. Метод addNote() должен добавлять записки в список notes.
+2. Метод removeNote() должен удалять записку из списка notes.
+3. Метод addNote() должен быть синхронизирован.
+4. Метод removeNote() должен быть синхронизирован.
 */
 
 public class Solution {
@@ -14,18 +20,18 @@ public class Solution {
     }
 
     public static class Note {
-
         public final List<String> notes = new ArrayList<String>();
 
-        public void addNote(int index, String note) {
+        public synchronized void addNote(int index, String note) {
             System.out.println("Сейчас будет добавлена заметка [" + note + "] На позицию " + index);
             notes.add(index, note);
             System.out.println("Уже добавлена заметка [" + note + "]");
         }
 
-        public void removeNote(int index) {
+        public synchronized void removeNote(int index) {
             System.out.println("Сейчас будет удалена заметка с позиции " + index);
-            String note = notes.remove(index);
+            String note;
+            note = notes.remove(index);
             System.out.println("Уже удалена заметка [" + note + "] с позиции " + index);
         }
     }
