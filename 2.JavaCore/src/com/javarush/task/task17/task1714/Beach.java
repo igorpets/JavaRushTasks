51e3284424da909be7559461bc651d2b8687f98c
Igor Pets <b_ear@mail.ru>
2022-09-28 00:24:19 +0300

    Task 1714 solved, 1722 loaded

--- 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -8,7 +8,7 @@ Comparable
 4. Все методы класса Beach, кроме метода main, должны быть синхронизированы.
 */
 
-public class Beach  implements Comparable{
+public class Beach implements Comparable<Beach> {
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -19,34 +19,36 @@ public class Beach  implements Comparable{
         this.quality = quality;
     }
 
-    public String getName() {
+    public synchronized String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public synchronized void setName(String name) {
         this.name = name;
     }
 
-    public float getDistance() {
+    public synchronized float getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public synchronized void setDistance(float distance) {
         this.distance = distance;
     }
 
-    public int getQuality() {
+    public synchronized int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    public synchronized void setQuality(int quality) {
         this.quality = quality;
     }
 
     public static void main(String[] args) {
-
     }
-    public int compareTo(Object obj){
-        return 0;
+
+    public synchronized int compareTo(Beach obj) {
+        int distance = Double.compare(this.distance, obj.distance);
+        int quality = Integer.compare(this.quality, obj.quality);
+        return quality - distance;
     }
 }
--- 2.JavaCore/src/com/javarush/task/task17/task1714/Beach_test.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach_test.java
@@ -4,6 +4,11 @@ public class Beach_test {
     public static void main(String[] args) {
         Beach b1 = new Beach("Лазурный", 300.5f, 4);
         Beach b2 = new Beach("Морской", 207.1f, 2);
-        System.out.println(b1.);
+        Beach b3 = new Beach("Песочный", 300.5f, 3);
+        Beach b4 = new Beach("Детское село", 207.1f, 3);
+        System.out.println(b1.compareTo(b2));
+        System.out.println(b2.compareTo(b1));
+        System.out.println(b3.compareTo(b4));
+        System.out.println(b4.compareTo(b3));
     }
 }
