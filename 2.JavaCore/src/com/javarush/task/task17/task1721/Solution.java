b7ed0ba9487408d1b5eee7b3f976036497feddb6
Пец Игорь Эдуардович <iepec@Eureca-corp.ru>
2022-09-30 15:52:00 +0300

    Task 1721 copy task info

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -8,6 +8,15 @@ import java.util.List;
 
 /* 
 Транзакционность
+1. Класс Solution должен содержать public static поле allLines типа List<String>.
+2. Класс Solution должен содержать public static поле forRemoveLines типа List<String>.
+3. Класс Solution должен содержать public void метод joinData() который может бросать исключение CorruptedDataException.
+4. Программа должна считывать c консоли имена двух файлов.
+5. Программа должна считывать построчно данные из первого файла в список allLines.
+6. Программа должна считывать построчно данные из второго файла в список forRemoveLines.
+7. Метод joinData должен удалить в списке allLines все строки из списка forRemoveLines, если в allLines содержаться все строки из списка forRemoveLines.
+8. Метод joinData должен очистить список allLines и выбросить исключение CorruptedDataException, если в allLines не содержаться все строки из списка forRemoveLines.
+9. Метод joinData должен вызываться в main.
 */
 
 public class Solution {
