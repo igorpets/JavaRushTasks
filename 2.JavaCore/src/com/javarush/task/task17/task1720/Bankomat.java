24bc3f4c327dc998e68250d547d7f3fe3cb651a4
Пец Игорь Эдуардович <iepec@Eureca-corp.ru>
2022-09-30 15:51:30 +0300

    Task 1720 solved, 1721 loaded

--- 2.JavaCore/src/com/javarush/task/task17/task1720/Bankomat.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1720/Bankomat.java
@@ -1,5 +1,12 @@
 package com.javarush.task.task17.task1720;
 
+/*
+1. +Класс BankAccount должен содержать private поле balance типа BigDecimal.
+2. +Метод deposit(String money) класса BankAccount должен делегировать поведение в метод deposit(BigDecimal money).
+3. +Метод withdraw(String money) класса BankAccount должен делегировать поведение в метод withdraw(BigDecimal money).
+4. В методе withdraw(BigDecimal money), если необходимо, используй synchronized.
+5. В методе deposit(BigDecimal money), если необходимо, используй synchronized.
+*/
 public class Bankomat {
 
     static BankAccount account = new BankAccount("Amigo");
@@ -31,7 +38,6 @@ public class Bankomat {
             }
         }
     };
-
 
     public static class SpendThread extends Thread {
 
