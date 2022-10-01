f6f1eb6f3ac4d919f42bb131dfe57f02b4d65d66
Igor Pets <b_ear@mail.ru>
2022-10-02 00:34:29 +0300

    Task 1806 solved, 1807 loaded

--- 2.JavaCore/src/com/javarush/task/task18/task1806/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1806/Solution.java
@@ -6,22 +6,27 @@ import java.io.IOException;
 
 /* 
 Исправить ошибки
+1. Программа должна использовать классы FileInputStream и FileOutputStream.
+2. Программа должна переписать все байты одного файла в другой одним куском.
+3. Потоки FileInputStream и FileOutputStream должны быть закрыты.
+4. Нужно исправить 4 ошибки.
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("c:/data.txt");
-        // Создаем поток-записи-байт-в-файл
-        FileOutputStream outputStream = new FileOutputStream("c:/result.txt");
+        try (FileInputStream inputStream = new FileInputStream("c:/data.txt");
+             // Создаем поток-записи-байт-в-файл
+             FileOutputStream outputStream = new FileOutputStream("c:/result.txt")) {
+            if (inputStream.available() > 0) {
+                //читаем весь файл одним куском
+                byte[] buffer = new byte[inputStream.available()];
+                int count = inputStream.read(buffer);
+                if (count > 0) {
+                    outputStream.write(buffer);
+                }
+            }
+        } catch (Exception e) {
 
-        if (inputStream.read() >= 0) {
-            //читаем весь файл одним куском
-            byte[] buffer = new byte[inputStream.available()];
-            int count = inputStream.read(buffer);
-            outputStream.write(buffer, 0, count);
         }
-
-        inputStream.reset();
-        outputStream.flush();
     }
 }
