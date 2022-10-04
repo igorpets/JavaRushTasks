3fe59550068cf48c79481a4b47aa840292e7020f
Igor Pets <b_ear@mail.ru>
2022-10-05 01:22:48 +0300

    Task 1820 solved, 1821 loaded

--- 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task18.task1820;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /* 
 Округление чисел
@@ -13,6 +19,28 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in);
+             BufferedReader reader = new BufferedReader(new FileReader(scan.nextLine()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(scan.nextLine()))) {
+            /*String line;
+            ArrayList<String> data = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals("")) {
+                    data.addAll(Arrays.stream(line.split(" ")).toList());
+                }
+            }*/
+            //writer.write(data.stream().mapToDouble(value->Double.parseDouble(value)).);
+            String numbers = reader.
+                    lines().
+                    flatMap(x -> Arrays.stream(x.split(" "))).
+                    mapToInt(value -> Math.round(Float.parseFloat(value))).
+                    mapToObj(String::valueOf).
+                    collect(joining(" "));
+
+            writer.write(numbers);
+        } catch (Exception e) {
+
+        }
 
     }
 }
