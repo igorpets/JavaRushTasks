4a5cc72748e1c8bf3b66cd6a7a06ec9c11c01ec7
Igor Pets <b_ear@mail.ru>
2022-10-05 22:45:33 +0300

    Task 1821 solved, 1822 loaded

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task18.task1821;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 
 /* 
 Встречаемость символов
@@ -9,6 +8,23 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) {
+        if (args.length < 1) return;
+        try (FileInputStream input = new FileInputStream(args[0])) {
+            int[] counts = new int[256];
+            byte[] data = new byte[input.available()];
+            input.read(data);
+            for (byte sym : data) {
+                counts[sym] += 1;
+            }
+            for (int i = 1; i < counts.length; i++) {
+                char chr = (char) i;
+                int count = counts[i];
+                if (count > 0)
+                    System.out.println(chr + " " + count);
+            }
+        } catch (
+                Exception e) {
 
+        }
     }
 }
