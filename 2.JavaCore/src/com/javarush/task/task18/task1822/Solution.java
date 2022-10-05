c9f139db618e71521371230dd50bd26cf616eb75
Igor Pets <b_ear@mail.ru>
2022-10-06 02:08:12 +0300

    Task 1822 solved, 1823 loaded

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -1,9 +1,7 @@
 package com.javarush.task.task18.task1822;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 /* 
 Поиск данных внутри файла
@@ -11,5 +9,16 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) {
+        if (args.length < 1) return;
+        try (Scanner scan = new Scanner(System.in);
+             BufferedReader reader = new BufferedReader(new FileReader(scan.nextLine()))) {
+            String target = args[0]+" ";
+            String line;
+            do {
+                line = reader.readLine();
+            } while (!line.startsWith(target));
+            if (line.startsWith(target))
+                System.out.println(line);
+        } catch (Exception e) {}
     }
 }
