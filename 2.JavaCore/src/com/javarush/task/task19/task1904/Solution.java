6087868400b2d2ff092b59ee58ee3d14f659396a
Igor Pets <b_ear@mail.ru>
2022-10-08 22:33:54 +0300

    Task 1904 release 1-5 points, 6 in progress

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task19.task1904;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -20,22 +24,46 @@ import java.util.Scanner;
 В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен
 читать данные только одного человека.
 
-1. PersonScanner должен быть интерфейсом.
-2. Класс PersonScannerAdapter должен реализовывать интерфейс PersonScanner.
-3. Класс PersonScannerAdapter должен содержать приватное поле fileScanner типа Scanner.
-4. Класс PersonScannerAdapter должен содержать конструктор с параметром Scanner.
-5. Метод close() класса PersonScannerAdapter должен делегировать полномочие такому же методу fileScanner.
+1. +PersonScanner должен быть интерфейсом.
+2. +Класс PersonScannerAdapter должен реализовывать интерфейс PersonScanner.
+3. +Класс PersonScannerAdapter должен содержать приватное поле fileScanner типа Scanner.
+4. +Класс PersonScannerAdapter должен содержать конструктор с параметром Scanner.
+5. +Метод close() класса PersonScannerAdapter должен делегировать полномочие такому же методу fileScanner.
 6. Метод read() класса PersonScannerAdapter должен читать строку с файла, парсить её, и возвращать данные
    только одного человека, в виде объекта класса Person.
 */
 
 public class Solution {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("input.txt");
+        Scanner scanner = new Scanner(path);
+        PersonScanner personScannerAdapter = new PersonScannerAdapter(scanner);
+        System.out.println(personScannerAdapter.read());
+        System.out.println(personScannerAdapter.read());
+        personScannerAdapter.close();
     }
 
-    public static class PersonScannerAdapter {
+    public static class PersonScannerAdapter implements PersonScanner {
+        private Scanner fileScanner;
+        public PersonScannerAdapter(Scanner scan) {
+            fileScanner = scan;
+        }
+        @Override
+        public Person read() throws IOException {
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileScanner.nextLine()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (Exception e) {
 
+            }
+            return null;
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+        }
     }
 }
