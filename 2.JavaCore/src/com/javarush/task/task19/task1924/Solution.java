c94be646363828dc521092cc0f741911fb9d6ea5
Igor Pets <b_ear@mail.ru>
2022-10-10 00:54:19 +0300

    Task 1924 solved

--- 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -10,20 +10,43 @@ import java.util.Map;
 
 /* 
 Замена чисел
-1. Класс Solution должен содержать публичное статическое поле map типа Map<Integer, String>, которое должно быть
+1. +Класс Solution должен содержать публичное статическое поле map типа Map<Integer, String>, которое должно быть
    сразу проинициализировано.
-2. Программа должна считывать имя файла с консоли (используй BufferedReader).
-3. BufferedReader для считывания данных с консоли должен быть закрыт.
-4. Программа должна считывать содержимое файла (используй FileReader).
-5. Поток чтения из файла (FileReader) должен быть закрыт.
+2. +Программа должна считывать имя файла с консоли (используй BufferedReader).
+3. +BufferedReader для считывания данных с консоли должен быть закрыт.
+4. +Программа должна считывать содержимое файла (используй FileReader).
+5. +Поток чтения из файла (FileReader) должен быть закрыт.
 6. Программа должна выводить в консоль все строки из файла, но числа должны быть заменены на слова из словаря map.
 7. Класс Solution должен содержать статический блок, в котором добавляются в map тринадцать пар.
 */
 
 public class Solution {
     public static Map<Integer, String> map = new HashMap<Integer, String>();
+    static {
+        map.put(0,"ноль");
+        map.put(1,"один");
+        map.put(2,"два");
+        map.put(3,"три");
+        map.put(4,"четыре");
+        map.put(5,"пять");
+        map.put(6,"шесть");
+        map.put(7,"семь");
+        map.put(8,"восемь");
+        map.put(9,"девять");
+        map.put(10,"десять");
+        map.put(11,"одиннадцать");
+        map.put(12,"двенадцать");
+    }
 
     public static void main(String[] args) {
-
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            String line;
+            while((line = reader.readLine())!=null) {
+                for(Map.Entry<Integer, String> entry:map.entrySet())
+                    line = line.replaceAll("\\b" + entry.getKey() + "\\b", entry.getValue());
+                System.out.println(line);
+            }
+        } catch (IOException e) {}
     }
 }
