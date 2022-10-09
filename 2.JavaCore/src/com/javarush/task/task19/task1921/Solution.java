bf6f577b026d07cc9cb1aa5af1f5f2f3bc065585
Igor Pets <b_ear@mail.ru>
2022-10-09 14:17:27 +0300

    Task 1921 solved, 1917 loaded

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -3,10 +3,7 @@ package com.javarush.task.task19.task1921;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.util.*;
 
 /* 
 Хуан Хуанович
@@ -26,17 +23,52 @@ import java.util.List;
 
 1. +Класс Solution должен содержать публичную константу PEOPLE типа List<Person>, которая должна быть
    сразу проинициализирована.
-2. Программа НЕ должна считывать данные с консоли.
-3. Программа должна считывать содержимое файла (используй FileReader).
-4. Поток чтения из файла (FileReader) должен быть закрыт.
-5. Программа должна заполнить список PEOPLE данными из файла.
-6. Программа должна правильно работать с двойными именами, например Анна-Надежда.
+2. +Программа НЕ должна считывать данные с консоли.
+3. +Программа должна считывать содержимое файла (используй FileReader).
+4. +Поток чтения из файла (FileReader) должен быть закрыт.
+5. +Программа должна заполнить список PEOPLE данными из файла.
+6. +Программа должна правильно работать с двойными именами, например Анна-Надежда.
 */
 
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
     public static void main(String[] args) {
+        if (args.length < 1) return;
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals("")) {
+                    String[] params = line.split(" ");
+                    if (params.length >= 4) {
+                        String name;
+                        Date bdate;
+                        if (true) {
+                            LinkedList<String> list = new LinkedList<>(Arrays.asList(params));
+                            Integer year = Integer.parseInt(list.pollLast());
+                            Integer month = Integer.parseInt(list.pollLast());
+                            Integer day = Integer.parseInt(list.pollLast());
+                            name = String.join(" ", list);
+                            bdate = new Date(year - 1900, month - 1, day);
+                        } else {
+                            name = "";
+                            String space = "";
+                            for (int i = 0; i < params.length - 3; i++) {
+                                name = name + space + params[i];
+                                if (space.equals("")) space = " ";
+                            }
+                            bdate = new Date(Integer.parseInt(params[params.length - 1]) - 1900,
+                                    Integer.parseInt(params[params.length - 2]) - 1,
+                                    Integer.parseInt(params[params.length - 3]));
+                        }
+                        PEOPLE.add(new Person(name, bdate));
+                    }
+                }
+            }
+            for (Person pers : PEOPLE)
+                System.out.println(pers.getName() + " " + pers.getBirthDate());
+        } catch (IOException e) {
+        }
     }
 }
