0c7e5f72dc765eb5042dd4ebafaaa82ed07d7477
Igor Pets <b_ear@mail.ru>
2022-10-15 01:54:14 +0300

    Task 2003 copy info

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -7,6 +7,14 @@ import java.util.Properties;
 
 /* 
 Знакомство с properties
+В методе main() происходит считывание пути к файлу с консоли и заполнение runtimeStorage данными из файла.
+Тебе необходимо в методе save() реализовать логику записи в файл для карты runtimeStorage, а в методе load() - логику чтения из файла для runtimeStorage.
+Файл должен быть в формате .properties. Комментарии в файле игнорируй.
+Про .properties прочитай в вики.
+Подсказка: используй объект класса Properties.
+
+1. Метод save() должен сохранять карту runtimeStorage в параметр outputStream.
+2. Метод load() должен восстанавливать состояние карты runtimeStorage из параметра inputStream.
 */
 
 public class Solution {
