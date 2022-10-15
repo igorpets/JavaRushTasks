7e3dcbc84006478aeb232a0f730346489cb5d02b
Igor Pets <b_ear@mail.ru>
2022-10-15 19:40:54 +0300

    Task 1902 copy info

--- 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -5,6 +5,14 @@ import java.io.IOException;
 
 /* 
 Адаптер
+1. AmigoStringWriter должен быть интерфейсом.
+2. Класс AdapterFileOutputStream должен реализовывать интерфейс AmigoStringWriter.
+3. Класс AdapterFileOutputStream должен содержать приватное поле fileOutputStream типа FileOutputStream.
+4. Класс AdapterFileOutputStream должен содержать конструктор с параметром FileOutputStream.
+5. Метод flush() класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу fileOutputStream.
+6. Метод writeString(String s) класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу
+   write() объекта fileOutputStream.
+7. Метод close() класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу fileOutputStream.
 */
 
 public class AdapterFileOutputStream {
