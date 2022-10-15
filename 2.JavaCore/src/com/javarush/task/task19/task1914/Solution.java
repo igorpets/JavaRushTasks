ea5fe0ac46682a98052be165cc8ef14266a7ccda
Igor Pets <b_ear@mail.ru>
2022-10-15 21:29:57 +0300

    Task 1914 solved, 1915 loaded

--- 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
@@ -5,12 +5,62 @@ import java.io.PrintStream;
 
 /* 
 Решаем пример
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна выводить на консоль решенный пример.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+
+Возможные операции: + - *
+Шаблон входных данных и вывода: a [знак] b = c
+Отрицательных и дробных чисел, унарных операторов - нет.
+
+Пример вывода:
+3 + 6 = 9
+
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу
+   проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "3 + 6 = ".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c
+   параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль
+   объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething()
+   согласно заданию, и выводить её в консоль.
 */
 
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream original_out = System.out;
+        ByteArrayOutputStream my_out = new ByteArrayOutputStream();
+        PrintStream my_print = new PrintStream(my_out);
+        System.setOut(my_print);
+        testString.printSomething();
+        //**********
+        String orig_text = my_out.toString().trim();
+        String[] data = orig_text.split(" ");
+        int arithmeticResult = 0;
+        int op1 = Integer.parseInt(data[0]);
+        int op2 = Integer.parseInt(data[2]);
+        switch (data[1]) {
+            case "+":
+                arithmeticResult = op1 + op2;
+                break;
+            case "-":
+                arithmeticResult = op1 - op2;
+                break;
+            case "*":
+                arithmeticResult = op1 * op2;
+                break;
+
+        }
+        //**********
+        System.setOut(original_out);
+        System.out.println(orig_text + " " + arithmeticResult);
     }
 
     public static class TestString {
