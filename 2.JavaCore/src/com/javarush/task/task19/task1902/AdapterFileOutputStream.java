230b79cfcaa63e4f9cc6be97d60507c439b89970
Igor Pets <b_ear@mail.ru>
2022-10-15 19:45:27 +0300

    Task 1902 solved, 1906 loaded

--- 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -7,32 +7,37 @@ import java.io.IOException;
 Адаптер
 +1. AmigoStringWriter должен быть интерфейсом.
 +2. Класс AdapterFileOutputStream должен реализовывать интерфейс AmigoStringWriter.
-3. Класс AdapterFileOutputStream должен содержать приватное поле fileOutputStream типа FileOutputStream.
-4. Класс AdapterFileOutputStream должен содержать конструктор с параметром FileOutputStream.
-5. Метод flush() класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу fileOutputStream.
-6. Метод writeString(String s) класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу
++3. Класс AdapterFileOutputStream должен содержать приватное поле fileOutputStream типа FileOutputStream.
++4. Класс AdapterFileOutputStream должен содержать конструктор с параметром FileOutputStream.
++5. Метод flush() класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу fileOutputStream.
++6. Метод writeString(String s) класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу
    write() объекта fileOutputStream.
-7. Метод close() класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу fileOutputStream.
++7. Метод close() класса AdapterFileOutputStream должен делегировать полномочие соответствующему методу fileOutputStream.
 */
 
-public class AdapterFileOutputStream implements AmigoStringWriter{
+public class AdapterFileOutputStream implements AmigoStringWriter {
+    private FileOutputStream fileOutputStream;
 
     public static void main(String[] args) {
 
     }
 
+    public AdapterFileOutputStream(FileOutputStream out) {
+        this.fileOutputStream = out;
+    }
+
     @Override
     public void flush() throws IOException {
-
+        fileOutputStream.flush();
     }
 
     @Override
     public void writeString(String s) throws IOException {
-
+        fileOutputStream.write(s.getBytes());
     }
 
     @Override
     public void close() throws IOException {
-
+        fileOutputStream.close();
     }
 }
