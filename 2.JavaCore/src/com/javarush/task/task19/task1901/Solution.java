4f7b354b9dc584806c0242df19a1d71c06da13df
Igor Pets <b_ear@mail.ru>
2022-10-15 19:35:57 +0300

    Task 1901 copy info and released 1-5 task points

--- 2.JavaCore/src/com/javarush/task/task19/task1901/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1901/Solution.java
@@ -2,6 +2,18 @@ package com.javarush.task.task19.task1901;
 
 /* 
 TableAdapter
+Измени класс TableAdapter так, чтобы он адаптировал ATable к BTable.
+Метод getHeaderText должен возвращать такую строку "[username] : tablename".
+
+Пример, "[Amigo] : DashboardTable".
+
++1. Класс Solution должен содержать public static интерфейс ATable.
++2. Класс Solution должен содержать public static интерфейс BTable.
++3. Класс Solution должен содержать public static класс TableAdapter.
++4. Класс TableAdapter должен реализовывать интерфейс BTable.
++5. Класс TableAdapter должен содержать приватное поле aTable типа ATable.
+6. Класс TableAdapter должен содержать конструктор с параметром ATable.
+7. Класс TableAdapter должен переопределять метод getHeaderText согласно заданию.
 */
 
 public class Solution {
@@ -23,8 +35,14 @@ public class Solution {
         System.out.println(table.getHeaderText());
     }
 
-    public static class TableAdapter {
+    public static class TableAdapter implements BTable {
+        private ATable aTable;
+        public TableAdapter(ATable aTable){
 
+        }
+        public String getHeaderText() {
+            return "";
+        }
     }
 
     public interface ATable {
