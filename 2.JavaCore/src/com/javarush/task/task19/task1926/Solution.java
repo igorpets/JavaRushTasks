066c7cdb874ad52d57f8b7da4cae024c7aad0bc7
Igor Pets <b_ear@mail.ru>
2022-10-16 14:22:06 +0300

    Task 1926 solved, 1927 loaded

--- 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -7,9 +7,20 @@ import java.io.InputStreamReader;
 
 /* 
 Перевертыши
+1. Программа должна считывать имя файла с консоли (используй BufferedReader).
+2. BufferedReader для считывания данных с консоли должен быть закрыт.
+3. Программа должна считывать содержимое файла (используй FileReader).
+4. Поток чтения из файла (FileReader) должен быть закрыт.
+5. Программа должна выводить в консоль все символы из файла в обратном порядке.
 */
 
 public class Solution {
     public static void main(String[] args) {
+        try(BufferedReader cons = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader reader = new BufferedReader(new FileReader(cons.readLine()))){
+            while(reader.ready()){
+                System.out.println(new StringBuilder(reader.readLine()).reverse());
+            }
+        }catch (IOException e){}
     }
 }
