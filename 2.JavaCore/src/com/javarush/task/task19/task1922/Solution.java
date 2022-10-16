35847f2f46fb2d346ec4a1cee2afc7fc26bf116e
Igor Pets <b_ear@mail.ru>
2022-10-16 13:21:40 +0300

    Task 1922 solved, 1923 loaded

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -5,10 +5,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /* 
 Ищем нужные строки
+Считать с консоли имя файла.
+Вывести в консоль все строки из файла, которые содержат всего 2 слова из списка words.
+Закрыть потоки.
+
+Пример:
+words содержит слова А, Б, В (количество слов в списке words может быть любым).
+
+Строки:
+В Б А Д //3 слова из words, не подходит
+А Б А Д //3 слова из words, не подходит
+Д А Д //1 слово из words, не подходит
+Д А Б Д //2 слова - подходит, выводим
+Д А А Д //2 слова - подходит, выводим
+
+
+Requirements:
+1. Класс Solution должен содержать публичное статическое поле words типа List<String>, которое должно быть
+   сразу проинициализировано.
+2. Класс Solution должен содержать статический блок, в котором добавляются три или больше слов в список words.
+3. Программа должна считывать имя файла с консоли (используй BufferedReader).
+4. BufferedReader для считывания данных с консоли должен быть закрыт.
+5. Программа должна считывать содержимое файла (используй FileReader).
+6. Поток чтения из файла (FileReader) должен быть закрыт.
+7. Программа должна выводить в консоль все строки из файла, которые содержат всего 2 слова из списка words.
+
+Пример:
+файл
+тут такой файл как вид
+вид вид
+вид вид тут
+В
+В В В
+В В опять
 */
 
 public class Solution {
@@ -21,6 +55,23 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int count = 0;
+                String[] line_words = line.split(" ");
+                for (String line_word: line_words) {
+                    for (String word : words) {
+                        if (word.equals(line_word)) count++;
+                        if (count > 2) break;
+                    }
+                }
+                if (count == 2) System.out.println(line);
+            }
+        } catch (IOException e) {
+
+        }
 
     }
 }
