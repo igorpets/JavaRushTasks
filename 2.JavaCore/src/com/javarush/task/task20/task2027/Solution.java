af30b611a70993685a65353786965bafcec4c199
Igor Pets <b_ear@mail.ru>
2022-10-20 23:21:36 +0300

    Task 2027 loaded and first version

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task20.task2027;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Кроссворд
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] crossword = new int[][]{
+                {'f', 'd', 'e', 'r', 'l', 'k'},
+                {'u', 's', 'a', 'm', 'e', 'o'},
+                {'l', 'n', 'g', 'r', 'o', 'v'},
+                {'m', 'l', 'p', 'r', 'r', 'h'},
+                {'p', 'o', 'e', 'e', 'j', 'j'}
+        };
+        detectAllWords(crossword, "home", "same");
+        /*
+Ожидаемый результат
+home - (5, 3) - (2, 0)
+same - (1, 1) - (4, 1)
+         */
+    }
+
+    public static List<Word> detectAllWords(int[][] crossword, String... words) {
+        for (String word:words){
+            char first = word.charAt(0);
+            int delta_x;
+            int delta_y;
+            for (int y=0;y<crossword.length; y++)
+                for(int x=0; x<crossword[y].length;x++){
+                    if (crossword[y][x] == first) {
+                        for (int dy=Math.max(0, y-1); dy<Math.max(crossword.length, y+1);dy++)
+                            ;//for
+                    }
+                }
+        }
+        return null;
+    }
+
+    public static class Word {
+        private String text;
+        private int startX;
+        private int startY;
+        private int endX;
+        private int endY;
+
+        public Word(String text) {
+            this.text = text;
+        }
+
+        public void setStartPoint(int i, int j) {
+            startX = i;
+            startY = j;
+        }
+
+        public void setEndPoint(int i, int j) {
+            endX = i;
+            endY = j;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - (%d, %d) - (%d, %d)", text, startX, startY, endX, endY);
+        }
+    }
+}
