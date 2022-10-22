ff122fb501af4c8d83e0549a23d10445c6c8ffd3
Igor Pets <b_ear@mail.ru>
2022-10-22 14:12:25 +0300

    Task 2101 solved

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2101/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2101/Solution.java
@@ -2,6 +2,22 @@ package com.javarush.task.task21.task2101;
 
 /* 
 Определяем адрес сети
+1. Даны IP-адрес и маска подсети, необходимо вычислить адрес сети - реализуй метод getNetAddress.
+Используй операцию поразрядной конъюнкции (логическое И).
+
+Пример:
+IP-адрес: 11000000 10101000 00000001 00000010 (192.168.1.2)
+Маска подсети: 11111111 11111111 11111110 00000000 (255.255.254.0)
+Адрес сети: 11000000 10101000 00000000 00000000 (192.168.0.0)
+
+2. Реализовать метод print, который выведет в консоль данные в двоичном коде. Для IP-адреса(192.168.1.2)
+должна быть выведена строка "11000000 10101000 00000001 00000010"
+3. Метод main не участвует в тестировании
+
+1. Метод getNetAddress должен вычислять и возвращать адрес сети согласно переданным параметрам(IP-адрес и маска подсети).
+2. Метод getNetAddress должен быть статическим и публичным.
+3. Метод print должен быть статическим и публичным.
+4. Метод print должен преобразовывать переданный ему IP адрес в двоичный код и выводить на экран(как в условии).
 */
 
 public class Solution {
@@ -15,9 +31,22 @@ public class Solution {
     }
 
     public static byte[] getNetAddress(byte[] ip, byte[] mask) {
-        return new byte[4];
+        if (ip == null || mask == null || ip.length < 4 || mask.length < 4) return null;
+        byte[] result = new byte[4];
+        for (int index = 0; index < 4; index++) {
+            result[index] = (byte) (ip[index] & mask[index]);
+        }
+        return result;
     }
 
     public static void print(byte[] bytes) {
+        String space = "";
+        for (byte b : bytes) {
+            int b2 = b & 0xFF;
+            String s2 = String.format("%8s", Integer.toBinaryString(b2)).replace(' ', '0');
+            System.out.print(space+s2);
+            space = " ";
+        }
+        System.out.println();
     }
 }
