84ee6ad097d6e8753a3099b4889fa0740b038155
Igor Pets <b_ear@mail.ru>
2022-10-22 14:40:59 +0300

    Task 2104 solved

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -5,8 +5,18 @@ import java.util.Set;
 
 /* 
 Equals and HashCode
-*/
+В классе Solution исправить пару методов equals/hashCode в соответствии с правилами реализации этих методов
+(детали уточни у своего любимого поисковика).
+Обе строки first и last должны принимать участие в сравнении с помощью метода equals и вычислении hashcode.
+Метод main не участвует в тестировании.
 
+1. Хешкоды одинаковых объектов должны быть равны.
+2. Метод equals должен проверять равен ли переданный объект текущему(сравнение через ==).
+3. Метод equals должен проверять является ли переданный объект объектом класса Solution.
+4. Метод equals должен возвращать true в случае, если поля first и last равны у переданного объекта и текущего
+   (не забудь что они могут быть равны null).
+5. Должно быть обеспечено корректное поведение HashSet с типом элементов Solution.
+*/
 public class Solution {
     private final String first, last;
 
@@ -15,12 +25,21 @@ public class Solution {
         this.last = last;
     }
 
-    public boolean equals(Solution n) {
-        return n.first.equals(first) && n.last.equals(last);
+    public boolean equals(Object n) {
+        if (n == null) return false;
+        if (this == n) return true;
+        if (n.getClass() != this.getClass()) return false;
+        Solution obj = (Solution) n;
+        if (obj.first == first && obj.last == last) return true;
+        if (obj.first == null || obj.last == null) return false;
+        return obj.first.equals(first) && obj.last.equals(last);
     }
 
     public int hashCode() {
-        return 31 * first.hashCode() + last.hashCode();
+        int res = 0;
+        if (first != null) res = 31 * first.hashCode();
+        if (last != null) res += last.hashCode();
+        return res;
     }
 
     public static void main(String[] args) {
