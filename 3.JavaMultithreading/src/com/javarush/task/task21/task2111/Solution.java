7af3e68fe7c60695cc2539228921b9f9b88bdf63
Igor Pets <b_ear@mail.ru>
2022-10-23 19:18:57 +0300

    Task 2111 copy info

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2111/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2111/Solution.java
@@ -9,6 +9,13 @@ import java.util.List;
 
 /* 
 Освобождаем ресурсы
+Реализуй метод finalize, предварительно обдумав, что именно в нем должно быть.
+Проведи рефакторинг метода getUsers в соответствии с java7 try-with-resources.
+
+1. Метод finalize в классе Solution должен содержать вызов super.finalize().
+2. Метод finalize в классе Solution должен корректно завершаться в случае, если значение поля connection равно null.
+3. Метод finalize в классе Solution должен закрывать текущее соединение, если значение поля connection не равно null.
+4. Метод getUsers должен корректно использовать try-with-resources.
 */
 
 public class Solution {
