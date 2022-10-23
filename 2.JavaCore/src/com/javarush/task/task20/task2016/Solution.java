941878d4828c7a59bfb75394005b3ceef5b9a2db
Igor Pets <b_ear@mail.ru>
2022-10-23 20:42:00 +0300

    Task 2116 solved

--- 2.JavaCore/src/com/javarush/task/task20/task2016/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2016/Solution.java
@@ -6,18 +6,18 @@ import java.io.Serializable;
 Минимум изменений
 Используя минимум изменений кода сделайте так, чтобы сериализация класса C стала возможной.
 
-1. В классе Solution должен существовать класс A.
-2. В классе Solution должен существовать класс B.
-3. В классе Solution должен существовать класс C.
-4. Класс B должен быть потомком класса A.
-5. Класс C должен быть потомком класса B.
-6. Класс A должен поддерживать интерфейс Serializable.
++1. В классе Solution должен существовать класс A.
++2. В классе Solution должен существовать класс B.
++3. В классе Solution должен существовать класс C.
++4. Класс B должен быть потомком класса A.
++5. Класс C должен быть потомком класса B.
++6. Класс A должен поддерживать интерфейс Serializable.
 7. Класс B не должен явно поддерживать интерфейс Serializable.
 8. Класс C не должен явно поддерживать интерфейс Serializable.
 */
 
 public class Solution {
-    public class A {
+    public class A implements Serializable {
         String name = "A";
 
         public A(String name) {
@@ -49,6 +49,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
+        C test = new Solution().new C("Игорь");
+        System.out.println(test.name + " " + test);
     }
 }
