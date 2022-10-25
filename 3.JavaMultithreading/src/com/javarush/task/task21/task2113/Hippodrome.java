1d1a994bbff4c396a4df89130e9c29c3b127059e
Igor Pets <b_ear@mail.ru>
2022-10-26 01:13:10 +0300

    Task 2113 Hippodrome step 8 passed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -3,12 +3,14 @@ package com.javarush.task.task21.task2113;
 /*
 Транзакционность
  */
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
     public static Hippodrome game;
     private List<Horse> horses;
+
     public static void main(String[] args) {
         List<Horse> h = new ArrayList<>();
         h.add(new Horse("Олень", 3, 0));
@@ -16,19 +18,28 @@ public class Hippodrome {
         h.add(new Horse("Мотильда", 3, 0));
         game = new Hippodrome(h);
     }
-    public Hippodrome(List<Horse> horses){
+
+    public Hippodrome(List<Horse> horses) {
         this.horses = horses;
     }
-    public List<Horse> getHorses(){
+
+    public List<Horse> getHorses() {
         return horses;
     }
-    public void run(){
+
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+
+    public void move() {
 
     }
-    public void move(){
 
-    }
-    public void print(){
+    public void print() {
 
     }
 }
