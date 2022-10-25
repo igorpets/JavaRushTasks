562caf98b9fa69fb29a4d35276c256e1870409c7
Igor Pets <b_ear@mail.ru>
2022-10-26 01:02:41 +0300

    Task 2113 Hippodrome step 5 passed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -1,10 +1,30 @@
 package com.javarush.task.task21.task2113;
 
 public class Horse {
-    String name;
-    double speed;
-    double distance;
+    private String name;
+    private double speed;
+    private double distance;
     public Horse(String name, double speed, double distance){
-
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public  void setSpeed(double speed){
+        this.speed = speed;
+    }
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getSpeed(){
+        return speed;
+    }
+    public double getDistance() {
+        return distance;
     }
 }
