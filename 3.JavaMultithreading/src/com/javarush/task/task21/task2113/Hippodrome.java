ad5792857fa452ae916192676220b83acb0e2e68
Igor Pets <b_ear@mail.ru>
2022-10-26 01:07:32 +0300

    Task 2113 Hippodrome step 6 passed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -3,12 +3,18 @@ package com.javarush.task.task21.task2113;
 /*
 Транзакционность
  */
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
+    public static Hippodrome game;
     private List<Horse> horses;
     public static void main(String[] args) {
-
+        List<Horse> h = new ArrayList<>();
+        h.add(new Horse("Олень", 3, 0));
+        h.add(new Horse("Бегун>", 3, 0));
+        h.add(new Horse("Мотильда", 3, 0));
+        game = new Hippodrome(h);
     }
     public Hippodrome(List<Horse> horses){
         this.horses = horses;
