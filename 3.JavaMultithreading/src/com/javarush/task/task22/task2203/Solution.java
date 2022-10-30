94f75fc768954c106e625799aedb75bf726e2b4b
Igor Pets <b_ear@mail.ru>
2022-10-30 11:15:16 +0300

    Task 2203 copy info

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -2,6 +2,14 @@ package com.javarush.task.task22.task2203;
 
 /* 
 Между табуляциями
+Метод getPartOfString должен возвращать подстроку между первой и второй табуляцией.
+На некорректные данные бросить исключение TooShortStringException.
+Класс TooShortStringException не менять.
+
+1. Класс TooShortStringException должен быть потомком класса Exception.
+2. Метод getPartOfString должен принимать строку в качестве параметра.
+3. В случае, если строка, переданная в метод getPartOfString содержит менее 2 табуляций должно возникнуть исключение TooShortStringException.
+4. Метод getPartOfString должен возвращать подстроку между первой и второй табуляцией.
 */
 
 public class Solution {
