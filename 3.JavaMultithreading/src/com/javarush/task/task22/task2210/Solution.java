978880e310028c4f87611b08a436f224154e10b0
Igor Pets <b_ear@mail.ru>
2022-11-03 00:24:13 +0300

    Task 2210 copy info

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -4,6 +4,10 @@ import java.util.StringTokenizer;
 
 /* 
 StringTokenizer
+1. Метод getTokens должен использовать StringTokenizer.
+2. Метод getTokens должен быть публичным.
+3. Метод getTokens должен принимать два параметра типа String.
+4. Массив типа String возвращенный методом getTokens должен быть заполнен правильно(согласно условию задачи).
 */
 
 public class Solution {
