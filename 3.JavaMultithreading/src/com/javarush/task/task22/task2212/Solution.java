07c6841ffd0bfdb39b4233a39bda6fb6ec155143
Igor Pets <b_ear@mail.ru>
2022-11-10 02:16:07 +0300

    Task 2212 update by Qestion by other user

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -21,7 +21,7 @@ package com.javarush.task.task22.task2212;
 */
 
 public class Solution {
-    public static boolean checkTelNumber(String telNumber) {
+    public static boolean checkTelNumber2(String telNumber) {
         if (telNumber == null || telNumber.length() == 0) return false;
         //Критерии валидности:
         int count_dig = 0;
@@ -57,19 +57,134 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        String str = "+380501234567";
-        if (checkTelNumber(str) != true)
-            System.out.println("Error: " + str);
-        str = "+38(050)1234567";
-        if (checkTelNumber(str) != true)
-            System.out.println("Error: " + str);
+        String[] strs_false = {"+38(050(1234567", "+38(050)123(456)7"};
+        System.out.println("\nВсё, что ниже, должно быть FALSE");
+        for (int i = 0; i < strs_false.length; i++)
+            System.out.println(strs_false[i] + " = " + checkTelNumber2(strs_false[i]));
 
-        String[] strs = {"+(380)501234567", "+3(805)01234567", "+38(050)1234567", "+380(501)234567",
+        String[] strs_true = {"+38(050)1234567", "+380501234567", "+(380)501234567", "+3(805)01234567",
+                "+38(050)1234567", "+380(501)234567",
                 "+3805(012)34567", "+38050(123)4567", "+380501(234)567", "+3805012(345)67", "+38050123(456)7"};
 
-        for (int i = 0; i < strs.length; i++) {
-            if (i == 0) System.out.println("\nВсё, что ниже, должно быть true");
-            System.out.println(strs[i] + " = " + checkTelNumber(strs[i]));
+        System.out.println("\nВсё, что ниже, должно быть TRUE");
+        for (int i = 0; i < strs_true.length; i++) {
+            System.out.println(strs_true[i] + " = " + checkTelNumber(strs_true[i]));
         }
+    }
+    public static boolean checkTelNumber(String telNumber) {
+        boolean result = false;
+
+        if (isNumberNotNull(telNumber) && isContainOnlyNumbersPlusAndBrackets(telNumber) && isEndedWithNumber(telNumber)) {
+            if (isStartedWithPlus(telNumber) && isContainTwelveNumbers(telNumber)) {
+                if (isContainBrackets(telNumber)) {
+                    if (isContainOnlyThreeNumbers(telNumber)) {
+                        result = true;
+                    }
+                } else {
+                    result = true;
+                }
+            } else if ((isStartedWithNumber(telNumber) || isStartedWithOpeningBracket(telNumber)) && isContainTenNumbers(telNumber)) {
+                if (isContainBrackets(telNumber) && isContainOnlyThreeNumbers(telNumber)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean isContainBrackets(String number) {
+        int countOfStartBracket = 0;
+        int countOfEndBracket = 0;
+
+        char[] chars = number.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(') {
+                countOfStartBracket++;
+            } else if (chars[i] == ')') {
+                countOfEndBracket++;
+            }
+        }
+
+        return (countOfStartBracket == 1 && countOfEndBracket == 1);
+    }
+
+    public static boolean isContainOnlyThreeNumbers(String number) {
+        boolean result = false;
+        char[] chars = number.toCharArray();
+
+        int firstBracketIndex = 0;
+        int secondBracketIndex = 0;
+
+        for (char ch : chars) {
+            if (ch == '(') {
+                firstBracketIndex = number.indexOf(ch);
+            } else if (ch == ')') {
+                secondBracketIndex = number.lastIndexOf(ch);
+            }
+        }
+
+        String code;
+        if (secondBracketIndex > firstBracketIndex) {
+            code = number.substring(firstBracketIndex + 1, secondBracketIndex);
+            result = (code.length() == 3);
+        }
+
+        return result;
+    }
+
+    public static boolean isStartedWithPlus(String number) {
+        String firstSymbol = number.substring(0, 1);
+        return firstSymbol.equals("+");
+    }
+
+    public static boolean isStartedWithNumber(String number) {
+        boolean result;
+        String firstSymbol = number.substring(0, 1);
+        try {
+            Integer.parseInt(firstSymbol);
+            result = true;
+        } catch (Exception e) {
+            result = false;
+        }
+        return result;
+    }
+
+    public static boolean isStartedWithOpeningBracket(String number) {
+        String firstSymbol = number.substring(0, 1);
+        return firstSymbol.equals("(");
+    }
+
+    public static boolean isContainOnlyNumbersPlusAndBrackets(String number) {
+        String replacedTelNumber = number.replaceAll("[^0-9()+]", "");
+        return number.equals(replacedTelNumber);
+    }
+
+    public static boolean isContainTwelveNumbers(String number) {
+        String replacedTelNumber = number.replaceAll("[^0-9]", "");
+        return replacedTelNumber.length() == 12;
+    }
+
+    public static boolean isContainTenNumbers(String number) {
+        String replacedTelNumber = number.replaceAll("[^0-9]", "");
+        return replacedTelNumber.length() == 10;
+    }
+
+    public static boolean isNumberNotNull(String number) {
+        return number != null;
+    }
+
+    public static boolean isEndedWithNumber(String number) {
+        boolean result;
+        String lastSymbol = number.substring(number.length() - 1);
+
+        try {
+            Integer.parseInt(lastSymbol);
+            result = true;
+        } catch (Exception e) {
+            result = false;
+
+        }
+
+        return result;
     }
 }
