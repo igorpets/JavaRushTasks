0a7f36a5b44798f7768816d6ed35e0000fbed416
Igor Pets <b_ear@mail.ru>
2022-11-13 01:56:01 +0300

    Task 2302 solved

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2302/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2302/Solution.java
@@ -2,12 +2,16 @@ package com.javarush.task.task23.task2302;
 
 /* 
 Запрети переопределение
+1. Класс Listener должен быть создан внутри класса Solution.
+2. Класс Listener должен быть публичным.
+3. Метод onMouseDown должен быть публичным.
+4. Должна быть запрещена возможность переопределить метод onMouseDown.
 */
 
 public class Solution {
 
     public static class Listener {
-        public void onMouseDown(int x, int y) {
+        final public void onMouseDown(int x, int y) {
             // Do something when the mouse down event occurs
         }
 
