d56a62441c3b5a05e054476bd4642213cba2feda
Igor Pets <b_ear@mail.ru>
2022-11-18 01:36:17 +0300

    Task 2305 solved

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -2,6 +2,15 @@ package com.javarush.task.task23.task2305;
 
 /* 
 Inner
+
+Реализовать метод getTwoSolutions, который должен возвращать массив из 2-х экземпляров класса Solution.
+Для каждого экземпляра класса Solution инициализировать поле innerClasses двумя значениями.
+Инициализация всех данных должна происходить только в методе getTwoSolutions.
+
+1. В классе Solution должен быть реализован метод getTwoSolutions.
+2. Метод getTwoSolutions должен быть статическим.
+3. Метод getTwoSolutions должен быть публичным.
+4. Метод getTwoSolutions должен возвращать массив типа Solution заполненный согласно заданию.
 */
 
 public class Solution {
@@ -11,8 +20,12 @@ public class Solution {
     }
 
     public static Solution[] getTwoSolutions() {
-
-        return null;
+        Solution[] res = {new Solution(), new Solution()};
+        for (Solution sol:res) {
+            InnerClass[] inner = {sol.new InnerClass(), sol.new InnerClass()};
+            sol.innerClasses = inner;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
