97aca8d2e590090e5449e2c3dd64d888a319ce39
Igor Pets <b_ear@mail.ru>
2022-11-18 01:28:38 +0300

    Task 2306 solved

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2306/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2306/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task23.task2306;
 
 /* 
 Inner 2
++ " I'm from " + city + " in the " + country + ".";
 */
 
 public class Solution {
@@ -33,7 +34,7 @@ public class Solution {
 
         //доступ к этому методу возможен только внутри класса Solution, т.к. модификатор иннер класса private
         public String getDescription() {
-            return String.format("My name is %s.", this.name);
+            return String.format("My name is %s. I'm from %s in %s.", this.name, city, country);
         }
     }
 
