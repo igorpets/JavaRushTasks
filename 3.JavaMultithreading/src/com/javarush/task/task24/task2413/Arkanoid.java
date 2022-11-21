d735f3731a95a9f958c2c72a175c1a8f7afe167b
Igor Pets <b_ear@mail.ru>
2022-11-22 00:37:20 +0300

    Task 2413 solved step 2

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,7 +1,21 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+    private int width;
+    private int height;
     public static void main(String[] args) {
 
+    }
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public void setWidth(int width){
+        this.width = width;
+    }
+    public void setHeight(int height){
+        this.height = height;
     }
 }
