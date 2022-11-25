9f4862ab5137cdfeb2cadb6ac682f17a39fb1efe
Igor Pets <b_ear@mail.ru>
2022-11-26 00:55:38 +0300

    Task 2413 solved step 7

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,4 +1,36 @@
 package com.javarush.task.task24.task2413;
-
+/*
+1. В классе BaseObject должно быть создано приватное поле x типа double.
+2. В классе BaseObject должно быть создано приватное поле y типа double.
+3. В классе BaseObject должно быть создано приватное поле radius типа double.
+4. В классе BaseObject должен быть создан корректно работающий конструктор с
+   тремя параметрами типа double (x, y, radius)
+ */
 public class BaseObject {
+   private double x;
+   private double y;
+   private double radius;
+   public  BaseObject(double x, double y, double radius){
+       this.x = x;
+       this.y = y;
+       this.radius = radius;
+   }
+   public double getX(){
+       return x;
+   }
+    public double getY(){
+        return y;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public void setX(double x){
+       this.x = x;
+    }
+    public void setY(double y){
+        this.y = y;
+    }
+    public void setRadius(double radius){
+       this.radius = radius;
+    }
 }
