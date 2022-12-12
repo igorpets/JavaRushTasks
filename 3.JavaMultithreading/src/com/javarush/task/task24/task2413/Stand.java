163336fe9653fdc87eb98b8c5777b1cfc787aa1b
Igor Pets <b_ear@mail.ru>
2022-12-13 00:43:01 +0300

    Task 2413 solved step 16

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,9 +1,27 @@
 package com.javarush.task.task24.task2413;
 
-public class Stand extends BaseObject{
-    public Stand(double x, double y, double radius){
+public class Stand extends BaseObject {
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y, double radius) {
         super(x, y, radius);
     }
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    public double getDirection() {
+        return this.direction;
+    }
+
     @Override
     public void draw(Canvas can) {
 
