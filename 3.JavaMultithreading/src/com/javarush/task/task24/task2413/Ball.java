576c9d94f8184ecc7f6805c04ddf8ec8df55f52d
Igor Pets <b_ear@mail.ru>
2022-12-13 00:39:16 +0300

    Task 2413 solved step 15

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,22 +1,9 @@
 package com.javarush.task.task24.task2413;
 
 /*
-1. Приватное поле speed типа double должно быть создано в классе Ball.
-2. Приватное поле direction типа double должно быть создано в классе Ball.
-3. Приватное поле dx типа double должно быть создано в классе Ball.
-4. Приватное поле dy типа double должно быть создано в классе Ball.
-5. Приватное поле isFrozen типа boolean должно быть создано в классе Ball.
-6. В классе Ball должен быть создан публичный конструктор (поведение и параметры описаны в условии).
-7. В классе Ball должен существовать корректный публичный геттер для поля speed.
-8. В классе Ball должен существовать корректный публичный геттер для поля direction.
-9. В классе Ball должен существовать корректный публичный геттер для поля dx.
-10. В классе Ball должен существовать корректный публичный геттер для поля dy.
-
-1. Метод move() в классе Ball должен увеличивать значение x на dx, если значение поля isFrozen равно false.
-2. Метод move() в классе Ball должен увеличивать значение y на dy, если значение поля isFrozen равно false.
-3. Метод move() в классе Ball не должен менять значения полей x и y, если значение поля isFrozen равно true.
-4. В классе Ball должен быть реализован метод draw(Canvas canvas) в соответствии с условием задачи.
-5. В классе Ball должен быть реализован метод start() устанавливающий значение поля isFrozen в false.
+1. В классе Ball должен быть создан метод void setDirection() c одним параметром типа double
+    (реализацию смотри в условии).
+2. В классе Ball должен быть создан метод void checkRebound c четырьмя параметрами типа int.
  */
 public class Ball extends BaseObject {
     private double speed;
@@ -30,6 +17,15 @@ public class Ball extends BaseObject {
         isFrozen = true;
     }
 
+    public void setDirection(double direction){
+        this.direction = direction;
+        double angle = Math.toRadians(direction);
+        dx = Math.cos(angle) * speed;
+        dy = -Math.sin(angle) * speed;
+    }
+    void checkRebound(int minx, int maxx, int miny, int maxy) {
+
+    }
     public void start() {
         isFrozen = false;
     }
