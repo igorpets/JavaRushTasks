b70f1bea81d077c0530fc6c15c70df622a1a998b
Igor Pets <b_ear@mail.ru>
2022-12-13 00:45:36 +0300

    Task 2413 solved step 17

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -29,6 +29,14 @@ public class Stand extends BaseObject {
 
     @Override
     public void move() {
+        x += speed * direction;
+    }
 
+    public void moveLeft() {
+        direction = -1;
+    }
+
+    public void moveRight() {
+        direction = 1;
     }
 }
