790fbc7376f8b5630f14dac997823f95dae95bba
Igor Pets <b_ear@mail.ru>
2023-01-06 01:06:00 +0300

    Task 2312 step 11 copy info

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -1,5 +1,11 @@
 package com.javarush.task.task23.task2312;
 
+/**
+ * 1. В классе Room должен быть создан метод createMouse.
+ * 2. В методе createMouse должна быть создана новая мышь по правилам описанным в условии и сохранена в поле mouse.
+ * 3. В классе Room должен быть создан метод eatMouse.
+ * 4. В методе eatMouse должен содержаться вызов метода createMouse.
+ */
 public class Room {
     private int width;
     private int height;
